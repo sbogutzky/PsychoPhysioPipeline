0c633b603a1856c308d053abad6480c1a305b526
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-04-03 19:56:50 +0200

    Package flow erstellt

---
+++ flow/R/flow.R
@@ -0,0 +1,12 @@
+#' flow: A package for non disruptive flow measures.
+#'
+#' The flow package provides three categories of important functions:
+#' Questionaire, HRV and Motion.
+#' 
+#' @section flow functions:
+#' add
+#' sum
+#'
+#' @docType package
+#' @name flow
+NULL
