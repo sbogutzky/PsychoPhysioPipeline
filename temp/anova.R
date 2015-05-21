4e8bcf1717527c1af995b947455a56bd5f04c680
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-05-21 16:27:09 +0200

    Anova bearbeitet

--- temp/anova.R
+++ temp/anova.R
@@ -1,26 +1,47 @@
-backup.anova  <- anova
-anova         <- backup.anova[1:6, 1:4]
-anova2        <- anova[1:6, 2:4]
-anova2        <- stack(anova2)
+# Repeated Anova Example
+# https://statistics.laerd.com/statistical-guides/repeated-measures-anova-statistical-guide.php
 
-subject       <- rep(anova$Subjects,3)  # create the "subject" variable
-anova2[3]     <- subject                # add it to the new data frame
-rm(subject)                             # clean up your workspace
-colnames(anova2)  <- c("v", "t", "s")   # rename the columns
-anova2$s <- as.factor(anova2$s)
+# Create Structure
+blood.pressure        <- read.csv("~/Entwicklung/bogutzky/repositories/non-disruptive-flow-measures/temp/blood-pressure.csv", sep=";")
+blood.pressure.2      <- blood.pressure[1:6, 2:4]
+blood.pressure.2      <- stack(blood.pressure.2)
+subject               <- rep(blood.pressure$Subjects, 3)  # create the "subject" variable
+blood.pressure.2[3]   <- subject                          # add it to the new data frame
+rm(subject)                                               # clean up your workspace
+colnames(blood.pressure.2)  <- c("blood.pressure", "time", "subject")   # rename the columns
+blood.pressure.2$subject    <- as.factor(blood.pressure.2$subject)
 
-with(anova2, tapply(v, t, sum))
+with(blood.pressure.2, tapply(blood.pressure, time, sum))
+
+# Check: Underlying Assumptions: Normality
 
 for(i in 1:3) {
-  print(names(anova)[i+1])
-  print(shapiro.test(anova[,i]))
-  qqnorm(anova[,i], ylab = names(anova)[i+1])
-  qqline(anova[,i])
+  print(names(blood.pressure)[i+1])
+  print(shapiro.test(blood.pressure[,i]))
+  qqnorm(blood.pressure[,i], ylab = names(blood.pressure)[i+1])
+  qqline(blood.pressure[,i])
 }
 
-aov.out       <- aov(v ~ t + Error(s/t), data=anova2)
+# The above table presents the results from two well-known tests of normality, namely the Kolmogorov-Smirnov Test and the Shapiro-Wilk Test. The Shapiro-Wilk Test is more appropriate for small sample sizes (< 50 samples), but can also handle sample sizes as large as 2000. For this reason, we will use the Shapiro-Wilk test as our numerical means of assessing normality.
+# We can see from the above table that for the "Pre.", "X3.Months" and "X3.Months" the dependent variable, "blood pressure", was normally distributed. How do we know this? If the Sig. value of the Shapiro-Wilk Test is greater than 0.05, the data is normal. If it is below 0.05, the data significantly deviate from a normal distribution.
+
+aov.out       <- aov(blood.pressure ~ time + Error(subject/time), data=blood.pressure.2)
 summary(aov.out)
 
+blood.pressure.value  <- blood.pressure.2$blood.pressure
+time                  <- blood.pressure.2$time
+subject               <- blood.pressure.2$subject
+data                  <- data.frame(subject, time, blood.pressure.value)
 
+matrix <- with(data, cbind(blood.pressure.value[time=="Pre."], blood.pressure.value[time=="X3.Months"], blood.pressure.value[time=="X6.Months"])) 
 
+model <- lm(matrix ~ 1)
+
+design <- factor(c("Pre.", "X3.Months", "X6.Months"))
+
+library(car)
+
+options(contrasts=c("contr.sum", "contr.poly"))
+aov <- Anova(model, idata=data.frame(design), idesign=~design, type="III")
+summary(aov, multivariate=F)
 
