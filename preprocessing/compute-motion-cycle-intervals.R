95e8f71cde27aaea118e39cfeaff41778f61246c
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-06-27 11:51:11 +0200

    Ordner angepasst

--- preprocessing/compute-motion-cycle-intervals.R
+++ preprocessing/compute-motion-cycle-intervals.R
@@ -7,16 +7,26 @@ require(signal)
 require(flow)
 require(RHRV)
 
-# Set network directory
-network.directory <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
+# Set root data directory path
+root.data.directory.path <- ""
+if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
+  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
 if(file.exists("/Volumes/flow/Documents/simon-bogutzky/data"))
-  network.directory <- "/Volumes/flow/Documents/simon-bogutzky/data/"
+  root.data.directory.path <- "/Volumes/flow/Documents/simon-bogutzky/data/"
+if(file.exists("//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"))
+  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
 
-# Set preprocessed data directory
-preprocessed.data.directory <- "./data/preprocessed-data/"
+# Set preprocessed data directory path
+preprocessed.data.directory.path <- "./data/preprocessed-data/"
+
+# Set processed data directory path
+processed.data.directory.path <- paste(root.data.directory.path, "processed-data/", sep = "")
+
+# Set features directory path
+features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
 
 # Load fss features
-fss.features        <- read.csv(paste(network.directory, "features/fss-features.csv", sep = ""), stringsAsFactors = F)
+fss.features <- read.csv(paste(features.directory.path, "fss-features.csv", sep = ""), stringsAsFactors = F)
 
 # Set body position
 body.position       <- "leg"
@@ -30,11 +40,11 @@ for (i in 1:nrow(fss.features)) {
   last.name       <- properties[, 6]
   first.name      <- properties[, 7]
   if(measurement == 1) {
-    date.directory  <- strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S")
+    date.directory  <- paste(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S"), "/", sep ="")
   }
   
   # Read motion data
-  motion.data.path <- paste(preprocessed.data.directory, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-motion-data-", measurement,  ".csv", sep="")
+  motion.data.path <- paste(preprocessed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, body.position, "-motion-data-", measurement,  ".csv", sep="")
   if(file.exists(motion.data.path)) {
   
     # Load motion data
@@ -81,7 +91,6 @@ for (i in 1:nrow(fss.features)) {
 
     # Compute time and intervals
     t.s               <- t[minima] / 1000
-    #cycle.interval.s  <- c(NA, diff(t.s))
     
     cycle.data          <- CreateHRVData()
     cycle.data          <- SetVerbose(cycle.data, T)
@@ -97,8 +106,8 @@ for (i in 1:nrow(fss.features)) {
     max.spm           <- m + 3 * s
     cycle.data        <- FilterNIHR(cycle.data, long=50, last=13, minbpm=min.spm, maxbpm=max.spm)
     
-    # Remove artefact manually
-    cycle.data          <- EditNIHR(cycle.data)
+    #TODO: Remove artefact manually
+    #cycle.data          <- EditNIHR(cycle.data)
     
     t.s               <- cycle.data$Beat$Time
     cycle.interval.s  <- cycle.data$Beat$RR / 1000
@@ -112,13 +121,13 @@ for (i in 1:nrow(fss.features)) {
     plot(t.s, cycle.interval.s, type="l", xlab="t [s]", ylab="Motion Cycle Interval [s]")
 
     # Create directory, if needed
-    output.directory <- paste(network.directory, "processed-data/", tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
-    if(!file.exists(output.directory)) {
-      dir.create(paste(output.directory, "/", sep = ""), recursive = TRUE)
+    output.directory.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
+    if(!file.exists(output.directory.path)) {
+      dir.create(output.directory.path, recursive = TRUE)
     }
 
     # Write csv file
-    output.file.path <- paste(output.directory, "/", body.position, "-motion-time-data-", measurement, ".csv", sep = "")
+    output.file.path <- paste(output.directory.path, "/", body.position, "-motion-time-data-", measurement, ".csv", sep = "")
     op <- options(digits.secs=3)
     con <- file(output.file.path, 'w') 
     writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d"), con = con)
