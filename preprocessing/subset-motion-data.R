724af19f1f5846fa8a49f98473a35239c689a266
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-06-27 11:39:56 +0200

    Order geändert und Achsentausch vorgenommen

--- preprocessing/subset-motion-data.R
+++ preprocessing/subset-motion-data.R
@@ -1,19 +1,29 @@
 # Remove all variables
-rm(list = ls(all = T))  
+rm(list = ls(all = T)) 
 
-# Set network directory
-network.directory <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"
+# Load libraries
+require(TSA)
+
+# Set root data directory path
+root.data.directory.path <- ""
+if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
+  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
 if(file.exists("/Volumes/flow/Documents/simon-bogutzky/data"))
-  network.directory <- "/Volumes/flow/Documents/simon-bogutzky/data"
+  root.data.directory.path <- "/Volumes/flow/Documents/simon-bogutzky/data/"
+if(file.exists("//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"))
+  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
 
-# Set network cleaned data directory
-cleaned.data.directory    <- paste(network.directory, "/cleaned-data/", sep = "")
+# Set cleaned data directory path
+cleaned.data.directory.path <- paste(root.data.directory.path, "cleaned-data/", sep = "")
 
-# Set local processed data directory
-processed.data.directory <- "./data/preprocessed-data/"
+# Set preprocessed data directory path
+preprocessed.data.directory.path <- "./data/preprocessed-data/"
+
+# Set features directory path
+features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
 
 # Load fss features
-fss.features <- read.csv(paste(network.directory, "/features/fss-features.csv", sep = ""), stringsAsFactors = F)
+fss.features <- read.csv(paste(features.directory.path, "fss-features.csv", sep = ""), stringsAsFactors = F)
 
 # Set body position
 body.position <- "leg"
@@ -28,14 +38,14 @@ for (i in 1:nrow(fss.features)) {
   last.name       <- properties[, 6]
   first.name      <- properties[, 7]
   date.of.birth   <- properties[, 8]
-  date.directory  <- strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S")
+  date.directory  <- paste(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S"), "/", sep = "")
   
   if(measurement == 1) {
     motion.data        <- data.frame()
     n               <- 0
     
     # Read data, if needed
-    motion.data.path <- paste(cleaned.data.directory, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-motion-data.csv", sep="")
+    motion.data.path <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-motion-data.csv", sep="")
     if(file.exists(motion.data.path)) {
       motion.data      <- read.csv(motion.data.path)
       
@@ -49,9 +59,9 @@ for (i in 1:nrow(fss.features)) {
       first.timestamp <- motion.data[1,8]
       
       # Create output directory, if needed
-      output.directory <- paste(processed.data.directory, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
-      if(!file.exists(output.directory)) {
-        dir.create(output.directory, recursive = TRUE)
+      output.directory.path <- paste(preprocessed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
+      if(!file.exists(output.directory.path)) {
+        dir.create(output.directory.path, recursive = TRUE)
       }
     } else {
       print("No motion data")
@@ -75,21 +85,43 @@ for (i in 1:nrow(fss.features)) {
     motion.rot.rate.y.deg.s  <- motion.data.subset[,6]
     motion.rot.rate.z.deg.s  <- motion.data.subset[,7]
     
-    if(mean(motion.accel.y.ms.2) < 0) {
+    motion.rot.rate.deg.s <- motion.rot.rate.x.deg.s
+    
+    # Find cyclic movement   
+    rotation.rate.x.spectrum <- TSA::periodogram(motion.rot.rate.x.deg.s, plot = F)
+    rotation.rate.y.spectrum <- TSA::periodogram(motion.rot.rate.y.deg.s, plot = F)
+    rotation.rate.z.spectrum <- TSA::periodogram(motion.rot.rate.z.deg.s, plot = F)
+    
+    axis <- which.max(c(max(rotation.rate.x.spectrum[[2]]), max(rotation.rate.y.spectrum[[2]]), max(rotation.rate.z.spectrum[[2]])))
+    
+    if(mean(motion.accel.y.ms.2) < 0 & axis == 1) {
       motion.accel.x.ms.2      <- -motion.accel.x.ms.2 
       motion.accel.y.ms.2      <- -motion.accel.y.ms.2
       motion.rot.rate.x.deg.s  <- -motion.rot.rate.x.deg.s
       motion.rot.rate.y.deg.s  <- -motion.rot.rate.y.deg.s
       
-      print("Axis switch!")
+      print("Axis switched cyclic motion around x")
+      
+      motion.rot.rate.deg.s <- motion.rot.rate.x.deg.s
+    }
+    
+    if(mean(motion.accel.y.ms.2) < 0 & axis == 3) {
+      motion.accel.z.ms.2      <- -motion.accel.z.ms.2 
+      motion.accel.y.ms.2      <- -motion.accel.y.ms.2
+      motion.rot.rate.z.deg.s  <- -motion.rot.rate.z.deg.s
+      motion.rot.rate.y.deg.s  <- -motion.rot.rate.y.deg.s
+      
+      print("Axis switched cyclic motion around z")
+      
+      motion.rot.rate.deg.s <- motion.rot.rate.z.deg.s
     } 
     
     # Plot data
-    plot(t.ms / 1000, motion.rot.rate.x.deg.s, type = "l", xlab = "t [s]", ylab = "Rotation Rate X [deg/s]", xlim = c(140, 150))
+    plot(t.ms / 1000, motion.rot.rate.deg.s, type = "l", xlab = "t [s]", ylab = "Rotation Rate [deg/s]", xlim = c(140, 150))
     title(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y/%m/%d %H:%M"))
     
     # Write csv file
-    output.file.path <- paste(output.directory, "/", body.position, "-motion-data-", measurement, ".csv", sep = "")
+    output.file.path <- paste(output.directory.path, body.position, "-motion-data-", measurement, ".csv", sep = "")
     write.csv(data.frame(t.ms, motion.accel.x.ms.2, motion.accel.y.ms.2, motion.accel.z.ms.2, motion.rot.rate.x.deg.s, motion.rot.rate.y.deg.s, motion.rot.rate.z.deg.s), output.file.path, row.names = FALSE)
     print(paste("Wrote:", output.file.path))
   }
