1d889cdfc70aee9a31569962ec95fa45697871ed
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-06-28 20:40:56 +0200

    Ornder angepasst

--- preprocessing/compute-motion-cycle-jerk-cost.R
+++ preprocessing/compute-motion-cycle-jerk-cost.R
@@ -1,95 +1,116 @@
 # Remove all variables
-rm(list = ls(all = T))  
-
-# Set working directory
-setwd("~/Entwicklung/bogutzky/repositories/non-disruptive-flow-measures/preprocessing")
+rm(list = ls(all = T)) 
 
 # Load libraries
 require(signal)
+require(flow)
 
-# Set properties
-root.data.path          <- "../data/preprocessed-data/"
-first.name              <- "Patrick"
-last.name               <- "Buse"
-activity                <- "Running"
-file.name.prefix        <- "leg"
-sampling.rate           <- 64
+# Set root data directory path
+root.data.directory.path <- ""
+if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
+  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
+if(file.exists("/Volumes/flow/Documents/simon-bogutzky/data"))
+  root.data.directory.path <- "/Volumes/flow/Documents/simon-bogutzky/data/"
+if(file.exists("//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"))
+  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
 
-# Load all file names
-data.path               <- paste(root.data.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/",  sep = "")
-motion.file.names       <- list.files(path = data.path, pattern = paste(file.name.prefix, "-motion-data-[1-9].csv", sep=""), recursive = T)
+# Set preprocessed data directory path
+preprocessed.data.directory.path <- "./data/preprocessed-data/"
 
-for (motion.file.name in motion.file.names) {
+# Set processed data directory path
+processed.data.directory.path <- paste(root.data.directory.path, "processed-data/", sep = "")
+
+# Set features directory path
+features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
+
+# Load fss features
+fss.features <- read.csv(paste(features.directory.path, "fss-features.csv", sep = ""), stringsAsFactors = F)
+
+# Set body position
+body.position       <- "leg"
+
+for (i in 1:nrow(fss.features)) {
   
-  # Load motion data
-  motion.data           <- read.csv(paste(data.path, motion.file.name, sep=""))
-  
-  # Extract properties
-  activity.start    <- as.POSIXct(strptime(regmatches(motion.file.name, regexpr("[0-9]{4}-[0-9]{2}-[0-9]{2}--[0-9]{2}-[0-9]{2}-[0-9]{2}", motion.file.name)), "%Y-%m-%d--%H-%M-%S"))
-  measurement       <- substr(regmatches(motion.file.name, regexpr("[1-9]{1}.csv", motion.file.name)), 1, 1)
-  
-  # Create directory, if needed
-  output.directory <- paste("../data/preprocessed-data/", tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", format(activity.start, format="%Y-%m-%d--%H-%M-%S", tz="CET"), "/", sep="")
-  if(!file.exists(output.directory)) {
-    dir.create(output.directory, recursive = TRUE)
+  properties      <- fss.features[i, c(6:12)]
+  activity        <- properties[, 1]
+  activity.start  <- properties[, 2]
+  measurement     <- properties[, 5]
+  last.name       <- properties[, 6]
+  first.name      <- properties[, 7]
+  if(measurement == 1) {
+    date.directory  <- paste(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S"), "/", sep ="")
   }
   
-  # Load motion time data
-  motion.time.data  <- read.csv(paste(output.directory, file.name.prefix, "-motion-time-data-", measurement, ".csv", sep = ""))
+  # Read motion data
+  motion.data.path <- paste(preprocessed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, body.position, "-motion-data-", measurement,  ".csv", sep="")
+  if(file.exists(motion.data.path)) {
+    
+    # Load motion data
+    motion.data <- read.csv(motion.data.path)
+    
+    # Number of data row
+    n <- nrow(motion.data)
   
-  # Compute jerk cost per cycle
-  # Isolate gravity from acceleration 
-  butterworth.filter  <- butter(1, 1/sampling.rate * 0.8, "low")
-  gravity.x           <- filtfilt(butterworth.filter, motion.data[, 2])
-  gravity.y           <- filtfilt(butterworth.filter, motion.data[, 3])
-  gravity.z           <- filtfilt(butterworth.filter, motion.data[, 4])
+    # Load motion time data
+    motion.time.data.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, body.position, "-motion-time-data-", measurement, ".csv", sep="")
+    motion.time.data      <- read.csv(motion.time.data.path)
+    
+    fs <- n / ((motion.data[n, 1] - motion.data[1, 1]) / 1000) 
+    # Compute jerk cost per cycle
+    # Isolate gravity from acceleration 
+    butterworth.filter  <- butter(1, 1/(fs/2) * 0.8, "low")
+    gravity.x           <- filtfilt(butterworth.filter, motion.data[, 2])
+    gravity.y           <- filtfilt(butterworth.filter, motion.data[, 3])
+    gravity.z           <- filtfilt(butterworth.filter, motion.data[, 4])
   
-  # Compute linear acceleration
-  linear.acceleration.x <- motion.data[, 2] - gravity.x
-  linear.acceleration.y <- motion.data[, 3] - gravity.y
-  linear.acceleration.z <- motion.data[, 4] - gravity.z
+    # Compute linear acceleration
+    linear.acceleration.x <- motion.data[, 2] - gravity.x
+    linear.acceleration.y <- motion.data[, 3] - gravity.y
+    linear.acceleration.z <- motion.data[, 4] - gravity.z
   
-  if (nrow(motion.time.data) > 1) {
-    jerk.costs    <- c()
-    for(i in 1:(nrow(motion.time.data) - 1)) {
+    if (nrow(motion.time.data) > 1) {
+      jerk.costs    <- c()
+        for(i in 1:(nrow(motion.time.data) - 1)) {
       
-      in.cycle <- motion.data[,1] >= motion.time.data[,1][i] & motion.data[,1] <= motion.time.data[,1][i+1]
+        in.cycle <- motion.data[,1] >= motion.time.data[,1][i] & motion.data[,1] <= motion.time.data[,1][i+1]
       
-      # Compute jerk cost of each cycle
-      t.subset                      <- motion.data[, 1][in.cycle]
-      linear.acceleration.x.subset  <- linear.acceleration.x[in.cycle]
-      linear.acceleration.y.subset  <- linear.acceleration.y[in.cycle]
-      linear.acceleration.z.subset  <- linear.acceleration.z[in.cycle]
+        # Compute jerk cost of each cycle
+        t.subset                      <- motion.data[, 1][in.cycle]
+        linear.acceleration.x.subset  <- linear.acceleration.x[in.cycle]
+        linear.acceleration.y.subset  <- linear.acceleration.y[in.cycle]
+        linear.acceleration.z.subset  <- linear.acceleration.z[in.cycle]
       
-      jerk.cost   <- CalculateJerkCost(t.subset, linear.acceleration.x.subset, linear.acceleration.y.subset, linear.acceleration.z.subset, normalized = T, plot = F)
-      jerk.costs  <- c(jerk.costs, jerk.cost)
-  
+        jerk.cost   <- CalculateJerkCost(t.subset, linear.acceleration.x.subset, linear.acceleration.y.subset, linear.acceleration.z.subset, normalized = T, plot = F)
+        jerk.costs  <- c(jerk.costs, jerk.cost)
+      }
+    } else {
+      print("Has no cycle indicators")
     }
-  } else {
-    print("Has no cycle indicators")
-  }
   
-  if (length(jerk.costs) > 0) {
-    jerk.costs <- c(NA, jerk.costs)
-    print(summary(jerk.costs))
-    par(mfcol=c(2, 1))
-    hist(jerk.costs)
-    t <- motion.time.data[,1]
-    plot(t, jerk.costs, type="l", xlab="t [s]", ylab="Jerk Cost [m^2 s^-5]")
+    if (length(jerk.costs) > 0) {
+      jerk.costs <- c(NA, jerk.costs)
+      print(summary(jerk.costs))
+      par(mfcol=c(2, 1))
+      hist(jerk.costs)
+      t.s <- motion.time.data[,1]
+      plot(t.s, jerk.costs, type="l", xlab="t [s]", ylab="Jerk Cost [m^2 s^-5]")
     
-    # Extract properties
-    activity.start    <- as.POSIXct(strptime(regmatches(motion.file.name, regexpr("[0-9]{4}-[0-9]{2}-[0-9]{2}--[0-9]{2}-[0-9]{2}-[0-9]{2}", motion.file.name)), "%Y-%m-%d--%H-%M-%S"))
-    measurement       <- substr(regmatches(motion.file.name, regexpr("[1-9]{1}.csv", motion.file.name)), 1, 1)
+      # Create directory, if needed
+      output.directory.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
+      if(!file.exists(output.directory.path)) {
+        dir.create(output.directory.path, recursive = TRUE)
+      }
     
-    # Create directory, if needed
-    output.directory <- paste("../data/preprocessed-data/", tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", format(activity.start, format="%Y-%m-%d--%H-%M-%S", tz="CET"), "/", sep="")
-    if(!file.exists(output.directory)) {
-      dir.create(output.directory, recursive = TRUE)
+      # Write csv file
+      output.file.path <- paste(output.directory.path, body.position, "-motion-jerk-cost-data-", measurement, ".csv", sep = "")
+      op <- options(digits.secs=3)
+      con <- file(output.file.path, 'w') 
+      writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d"), con = con)
+      writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%H:%M:%OS"), con = con)
+      write.csv(data.frame(t.s, jerk.cost.m.2.s..5 = jerk.costs), file = con, row.names = FALSE)
+      close(con)
+      options(op) #reset options
+      print(paste("Wrote:", output.file.path))
     }
-    
-    # Write csv file
-    output.file.path <- paste(output.directory, file.name.prefix, "-motion-jerk-cost-data-", measurement, ".csv", sep = "")
-    write.csv(data.frame(t, jerk.costs), output.file.path, row.names = FALSE)
-    print(output.file.path)
   }
 }
