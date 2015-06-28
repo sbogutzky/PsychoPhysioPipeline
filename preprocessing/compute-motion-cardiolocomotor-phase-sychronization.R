b77e777ecdf05f3b5a9cb7f202f451ffa8848fba
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-06-28 17:45:44 +0200

    CPS Berechnung fertig gestellt

--- preprocessing/compute-motion-cardiolocomotor-phase-sychronization.R
+++ preprocessing/compute-motion-cardiolocomotor-phase-sychronization.R
@@ -48,14 +48,14 @@ for (i in 1:nrow(fss.features)) {
     hrv.time.data <- read.csv(hrv.time.data.path, skip = 2)
     
     # Plot
-    par("mfcol" = c(3, 1), mar = c(2.5, 2.5, .5, 3.5) + 0.1, mgp = c(1.5, .5, 0), las = 1, cex.axis = 0.8, tck = .03, cex.lab = .8, xaxs = "i", yaxs = "i")
+    par("mfcol" = c(4, 1), mar = c(2.5, 2.5, .5, 3.5) + 0.1, mgp = c(1.5, .5, 0), las = 1, cex.axis = 0.8, tck = .03, cex.lab = .8, xaxs = "i", yaxs = "i")
     
     # BPM vs. SPM
     t.l       <- motion.time.data$t.s
     t.c       <- hrv.time.data$t.s
     spm       <- 120 / motion.time.data$cycle.interval.s
     bpm       <- 60 / hrv.time.data$rr.interval.s
-    x.lim      <- c(0, 900)
+    x.lim     <- c(min(t.c), max(t.c))
     plot(t.l, spm, xlab = "", ylab = "Cadence & HR", pch = 21, bg = "#3FADCB", cex = .5, xlim = x.lim, xaxt = "n")
     points(t.c, bpm, pch = 22, bg = "#33D100", cex = .5)
     abline(v = seq(x.lim[1], x.lim[2], 20), lty = "dashed", col = "lightgrey")
@@ -67,7 +67,7 @@ for (i in 1:nrow(fss.features)) {
     fi  <- CalculateInstantaneousPhases(t.c, t.l)
     m   <- .5
     psi <- fi %% (2 * pi * m)
-    y.lim      <- c(0, 1)
+    y.lim <- c(0, 1)
     plot(t.c, psi / (2 * pi * m), xlab = "", ylab = expression(Psi[m](tk) / (2 * pi * m)), xaxt = "n",  yaxt = "n", xlim = x.lim, ylim = y.lim, pch = 21, bg = "#CB3FAD") # bg = rep(c("#3FADCB", "#CB3FAD"), length(psi)))
     abline(v = seq(x.lim[1], x.lim[2], 20), lty = "dashed", col = "lightgrey")
     axis(1, at = seq(x.lim[1], x.lim[2], 20), labels = seq(x.lim[1], x.lim[2], 20), las = 1)
@@ -75,8 +75,8 @@ for (i in 1:nrow(fss.features)) {
     box()
     
     # Indexes
-    t.w <- 10
-    t.s <- seq(t.w/2, 900 - t.w/2, 1)
+    t.w <- 20
+    t.s <- seq(t.w/2, x.lim[2] - t.w/2, 1)
     phase.coherence.indexes <- c()
     phase.normalized.shannon.entropy.indexes <- c()
     for (t in t.s) {
@@ -88,21 +88,40 @@ for (i in 1:nrow(fss.features)) {
     abline(v = seq(x.lim[1], x.lim[2], 20), lty = "dashed", col = "lightgrey")
     axis(1, at = seq(x.lim[1], x.lim[2], 20), labels = seq(x.lim[1], x.lim[2], 20), las = 1)
     axis(2, at = seq(y.lim[1], y.lim[2], .2), labels = seq(y.lim[1], y.lim[2], .2))
-    legend("topleft", c("Phase Coherence Index", "Shannon Entropy Index"), lty = c("solid", "dashed"), col = c("#3FADCB", "#000000"), cex = .8, bg = "white")
+    legend("bottomright", c("Phase Coherence Index", "Shannon Entropy Index"), lty = c("solid", "dashed"), col = c("#3FADCB", "#000000"), cex = .8, bg = "white")
     box()
     
+    hist(psi / (2 * pi * m), breaks = seq(0, 1, .05), col = "black", border = "white", main = "", xlab = expression(Psi[m] / (2 * pi * m)), ylab = "")
     
-    t.j <- t.k - t.w / 2 <= t & t < t.k + t.w / 2
-    m <- length(psi[t.j])
-    if(is.na(bins))
-      bins <- exp(0.626 + 0.4 * log(m - 1))
-    #print(paste("Number of bins:", bins))
-    h   <- hist(psi[t.j], breaks = seq(0, 2 * pi, 2 * pi / bins), plot = F)
-    p.j <- h$counts / bins
-    h   <- sum(p * log(p), na.rm = T) / bins
-    h.max <- log(bins)
-    return((h.max - h) / h.max)
+    # Create directory, if needed
+    output.directory.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
+    if(!file.exists(output.directory.path)) {
+      dir.create(output.directory.path, recursive = TRUE)
+    }
     
-    # hist(psi / (2 * pi * m), breaks = seq(0, 1, .05), col = "black", border = "white", main = "", xlab = expression(Psi[m] / (2 * pi * m)), ylab = "")
+    # Write csv file
+    output.file.path <- paste(output.directory.path, body.position, "-cps-indexes-", measurement, ".csv", sep = "")
+    op <- options(digits.secs=3)
+    con <- file(output.file.path, 'w') 
+    writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d"), con = con)
+    writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%H:%M:%OS"), con = con)
+    write.csv(data.frame(t.s, phase.coherence.index = phase.coherence.indexes, phase.normalized.shannon.entropy.index = phase.normalized.shannon.entropy.indexes), file = con, row.names = FALSE)
+    close(con)
+    options(op) #reset options
+    print(paste("Wrote:", output.file.path))
+    
+    # Write csv file
+    output.file.path <- paste(output.directory.path,  body.position, "-cps-index-", measurement, ".csv", sep = "")
+    op <- options(digits.secs=3)
+    con <- file(output.file.path, 'w') 
+    writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d"), con = con)
+    writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%H:%M:%OS"), con = con)
+    write.csv(data.frame(t.s = t.c, psi.normalized.rad = psi / (2 * pi * m)), file = con, row.names = FALSE)
+    close(con)
+    options(op) #reset options
+    print(paste("Wrote:", output.file.path))
+    
+  } else {
+    print("No data")
   }
 }
