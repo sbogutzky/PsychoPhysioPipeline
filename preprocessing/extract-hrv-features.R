2d72c219359b04bb9d5a5e9a40cb7096ea771ce2
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-06-29 19:55:34 +0200

    Berechnung der HRV Parameter abgepasst

--- preprocessing/extract-hrv-features.R
+++ preprocessing/extract-hrv-features.R
@@ -1,52 +1,59 @@
 # Remove all variables
-rm(list = ls(all = T))  
-
-# Set working directory
-setwd("~/Entwicklung/bogutzky/repositories/non-disruptive-flow-measures/preprocessing")
+rm(list = ls(all = T)) 
 
 # Load libraries
-library(RHRV)
+require(RHRV)
 
-# Set paths
-preprocessed.data.path  <- "../data/preprocessed-data/"
+# Set root data directory path
+root.data.directory.path <- ""
+if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
+  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
+if(file.exists("/Volumes/flow/Documents/simon-bogutzky/data"))
+  root.data.directory.path <- "/Volumes/flow/Documents/simon-bogutzky/data/"
+if(file.exists("//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"))
+  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
 
-# How many minutes before the activity end the measurement started?
-measurement.started.before  <- 5
+# Set preprocessed data directory path
+preprocessed.data.directory.path <- "./data/preprocessed-data/"
 
-# How many minutes before the activity end the measurement ended?
-measurement.ended.before  <- 0
+# Set processed data directory path
+processed.data.directory.path <- paste(root.data.directory.path, "processed-data/", sep = "")
+
+# Set features directory path
+features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
 
 # Load fss features
-fss.features        <- read.csv("../data/features/fss-features.csv", stringsAsFactors = F)
+fss.features <- read.csv(paste(features.directory.path, "fss-features.csv", sep = ""), stringsAsFactors = F)
 
-# Create feature data frame as result
+# Create hrv feature data frame
 hrv.features  <- data.frame()
 
 for (i in 1:nrow(fss.features)) {
+  
   properties      <- fss.features[i, c(6:13)]
   activity        <- properties[, 1]
   activity.start  <- properties[, 2]
-  activity.end    <- properties[, 3]
+  activity.end    <- properties[, 3]   
   measurement     <- properties[, 5]
   last.name       <- properties[, 6]
   first.name      <- properties[, 7]
   date.of.birth   <- properties[, 8]
-  
   if(measurement == 1) {
-    current.data.path   <- paste(preprocessed.data.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", strftime(activity.start, format = "%Y-%m-%d--%H-%M-%S"), "/",  sep = "")
+    date.directory  <- paste(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S"), "/", sep ="")
   }
-  current.file.name   <- paste("ecg-data-", measurement, "_hrv.txt", sep = "")
-  current.file.path   <- paste(current.data.path, current.file.name, sep = "")
   
-  if(file.exists(current.file.path)) {
+  hrv.time.data.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "hrv-time-data-", measurement, ".csv", sep="")
+  if(file.exists(hrv.time.data.path)) {
     
-    # Load beat times from KubiosHRV txt file
-    beat.times <- read.csv(current.file.path, header = F, na.strings = "", fill = T, skip = 117, stringsAsFactors = F, col.names = c("", "Time", "RR.interval", "FFT.Frequency", "FFT.PSD", "AR.Frequency", "AR.PSD", "VLF.comp.", "LF.comp.", "HF.comp.", ""))[,2]
-    beat.times <- beat.times[complete.cases(beat.times)]
+    # Load time data
+    hrv.time.data <- read.csv(hrv.time.data.path, skip = 2)
+    
+    # Set beat times
+    beat.times <- hrv.time.data[, 1]
     
     # Create hrv data
     hrv.data          <- CreateHRVData()
-    hrv.data          <- SetVerbose(hrv.data, F)
+    hrv.data          <- SetVerbose(hrv.data, T)
     hrv.data$Beat     <- data.frame("Time" = beat.times)
     
     # Build not interpolated heart rates
@@ -70,7 +77,8 @@ for (i in 1:nrow(fss.features)) {
     #PlotHR(hrv.data)
     
     # Create time analysis
-    #hrv.data          <- CreateTimeAnalysis(hrv.data, size=60)
+    hrv.data          <- CreateTimeAnalysis(hrv.data)
+    
     
     # Plot spectogram with Short-time Fourier transform 30 seconds window with 1 second shift for LF
     #PlotSpectrogram(hrv.data, size=30, shift=1, freqRange=c(0.04, 0.15))
@@ -106,19 +114,22 @@ for (i in 1:nrow(fss.features)) {
     # Clean up
     rm(ulf.power.a, vlf.power.a, lf.power.a, hf.power.a, total.power, ulf.power.r, vlf.power.r, lf.power.r, hf.power.r, lf.power.nu, hf.power.nu, lfhf, mean.hr)
     
-    # Compute start and end of the measurement
-    measurement.start <- strftime(as.POSIXct(activity.end) - measurement.started.before * 60, format = "%Y-%m-%d %H:%M:%S")
-    measurement.end   <- strftime(as.POSIXct(activity.end) - measurement.started.before * 60, format = "%Y-%m-%d %H:%M:%S")
-    
     # Add parameter to feature vector
-    hrv.features    <- rbind(hrv.features, data.frame(round(hrv.parameters, 2), activity, activity.start, activity.end, measurement.start, measurement.end, measurement, last.name, first.name, date.of.birth))
+    hrv.features    <- rbind(hrv.features, data.frame(round(hrv.parameters, 2), activity, activity.start, activity.end, measurement, last.name, first.name, date.of.birth))
   }
 }
 
+# Create output directory, if needed
+output.directory.path <- features.directory.path
+if(!file.exists(output.directory.path)) {
+  dir.create(output.directory.path, recursive = TRUE)
+}
+
 # Write to csv file
-if(file.exists("../data/features/hrv-features.csv")) {
-  features <- read.csv("../data/features/hrv-features.csv", stringsAsFactors = F)
-  write.csv(rbind(features, hrv.features), "../data/features/hrv-features.csv", row.names = F)
+output.file.path <- paste(output.directory.path, "hrv-features.csv", sep = "")
+if(file.exists(output.file.path)) {
+  features <- read.csv(output.file.path, stringsAsFactors = FALSE)
+  write.csv(unique(rbind(features, hrv.features)), output.file.path, row.names = FALSE)
 } else {
-  write.csv(hrv.features, "../data/features/hrv-features.csv", row.names = F)
+  write.csv(hrv.features, output.file.path, row.names = FALSE)
 }
