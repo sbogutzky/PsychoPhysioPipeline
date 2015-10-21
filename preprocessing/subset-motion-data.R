7d7330f805fc1b211dc3745f3b86bc41147b9291
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-10-21 16:31:05 +0200

    Verbesserungen und ReadLine eingefügt

--- preprocessing/subset-motion-data.R
+++ preprocessing/subset-motion-data.R
@@ -6,62 +6,66 @@ require(TSA)
 
 # Set root data directory path
 root.data.directory.path <- ""
-if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
-  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
 if(file.exists("/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
-  root.data.directory.path <- "/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
+  root.data.directory.path        <- "/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
 if(file.exists("//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
-  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
+  root.data.directory.path        <- "//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
 
 # Set cleaned data directory path
-cleaned.data.directory.path <- paste(root.data.directory.path, "cleaned-data/", sep = "")
-
-# Set preprocessed data directory path
-preprocessed.data.directory.path <- "./data/preprocessed-data/"
+cleaned.data.directory.path       <- paste(root.data.directory.path, "cleaned-data/", sep = "")
 
 # Set features directory path
-features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
+features.directory.path           <- paste(root.data.directory.path, "features/", sep = "")
+
+# Set preprocessed data directory path
+preprocessed.data.directory.path  <- "./data/preprocessed-data/"
+
+# Read activity directory
+activity.directory  <- readline("Type in activity directory and press return to continue (e. g. walking/) > ")
+
+# Read user directory
+user.directory      <- readline("Type in user directory and press return to continue (e. g. john-doe/) > ")
+
+# Read in body position
+body.position       <- readline("Type in body position and press return to continue > (e. g. leg) > ")
 
 # Load fss features
-fss.features <- read.csv(paste(features.directory.path, "fss-features.csv", sep = ""), stringsAsFactors = F)
+fss.features <- read.csv(paste(features.directory.path, activity.directory, user.directory, "fss-features.csv", sep = ""), stringsAsFactors = F)
 
-# Set body position
-body.position <- "leg"
-
-for (i in 1:nrow(fss.features)) {
+for (i in 1:1) {
   
   properties      <- fss.features[i, c(6:13)]
   activity        <- properties[, 1]
   activity.start  <- properties[, 2]
   activity.end    <- properties[, 3]
   measurement     <- properties[, 5]
-  last.name       <- properties[, 6]
-  first.name      <- properties[, 7]
-  date.of.birth   <- properties[, 8]
+#   last.name       <- properties[, 6]
+#   first.name      <- properties[, 7]
+#   date.of.birth   <- properties[, 8]
   date.directory  <- paste(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S"), "/", sep ="")
   if(measurement == 1) {
     print(date.directory)
     motion.data     <- data.frame()
     n               <- 0
     
-    if(body.position == "arm") {
-      acceleration.data.path <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-accelerometer-data.csv", sep="")
-      gyroscope.data.path    <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-gyroscope-data.csv", sep="")
-    
-      if(file.exists(acceleration.data.path) & file.exists(gyroscope.data.path)) {
-        acceleration.data <- read.csv(acceleration.data.path)
-        gyroscope.data    <- read.csv(gyroscope.data.path)
-        gyroscope.data    <- gyroscope.data[,1:4] 
-        
-        motion.data <- merge(acceleration.data, gyroscope.data, by.x = "Timestamp", by.y = "Timestamp")
-        motion.data <- motion.data[, c(1:4, 6:8, 5)]
-        motion.data[,1] <- motion.data[,1] / (10^6)
-      }
-    } else {
-      motion.data.path <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, body.position, "-motion-data.csv", sep="")
+#     if(body.position == "arm") {
+#       acceleration.data.path <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-accelerometer-data.csv", sep="")
+#       gyroscope.data.path    <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "/", body.position, "-gyroscope-data.csv", sep="")
+#     
+#       if(file.exists(acceleration.data.path) & file.exists(gyroscope.data.path)) {
+#         acceleration.data <- read.csv(acceleration.data.path)
+#         gyroscope.data    <- read.csv(gyroscope.data.path)
+#         gyroscope.data    <- gyroscope.data[,1:4] 
+#         
+#         motion.data <- merge(acceleration.data, gyroscope.data, by.x = "Timestamp", by.y = "Timestamp")
+#         motion.data <- motion.data[, c(1:4, 6:8, 5)]
+#         motion.data[,1] <- motion.data[,1] / (10^6)
+#       }
+#     } else {
+      motion.data.path <- paste(cleaned.data.directory.path, activity.directory, user.directory, date.directory, body.position, "-motion-data.csv", sep="")
       if(file.exists(motion.data.path)) {
         motion.data      <- read.csv(motion.data.path)
-      }
+      # }
     }
       
     # Number of data rows
@@ -75,7 +79,7 @@ for (i in 1:nrow(fss.features)) {
       first.timestamp <- motion.data[1,8]
       
       # Create output directory, if needed
-      output.directory.path <- paste(preprocessed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
+      output.directory.path <- paste(preprocessed.data.directory.path, activity.directory, user.directory, date.directory, sep="")
       if(!file.exists(output.directory.path)) {
         dir.create(output.directory.path, recursive = TRUE)
       }
@@ -87,8 +91,8 @@ for (i in 1:nrow(fss.features)) {
   if(n > 0) {
     
     # Subset motion data
-    motion.data.subset   <- motion.data[activity.start <= first.timestamp + motion.data[,1] & first.timestamp + motion.data[,1] < activity.end,]
-    time.difference   <- first.timestamp + motion.data.subset[1,1] - activity.start
+    motion.data.subset  <- motion.data[activity.start <= first.timestamp + motion.data[,1] & first.timestamp + motion.data[,1] < activity.end,]
+    time.difference     <- first.timestamp + motion.data.subset[1,1] - activity.start
     print(paste("Time difference (ms):", round(time.difference, 3)))
     print(paste("Total time      (ms):", round(motion.data.subset[nrow(motion.data.subset),1] - motion.data.subset[1,1] + time.difference, 3)))
     
@@ -110,26 +114,27 @@ for (i in 1:nrow(fss.features)) {
     
     axis <- which.max(c(max(rotation.rate.x.spectrum[[2]]), max(rotation.rate.y.spectrum[[2]]), max(rotation.rate.z.spectrum[[2]])))
     
+    #TODO: Control axis switch
     if(mean(motion.accel.y.ms.2) < 0 & axis == 1) {
-      motion.accel.x.ms.2      <- -motion.accel.x.ms.2 
-      motion.accel.y.ms.2      <- -motion.accel.y.ms.2
-      motion.rot.rate.x.deg.s  <- -motion.rot.rate.x.deg.s
-      motion.rot.rate.y.deg.s  <- -motion.rot.rate.y.deg.s
+#       motion.accel.x.ms.2      <- -motion.accel.x.ms.2 
+#       motion.accel.y.ms.2      <- -motion.accel.y.ms.2
+#       motion.rot.rate.x.deg.s  <- -motion.rot.rate.x.deg.s
+#       motion.rot.rate.y.deg.s  <- -motion.rot.rate.y.deg.s
       
       print("Axis switched cyclic motion around x")
       
-      motion.rot.rate.deg.s <- motion.rot.rate.x.deg.s
+      # motion.rot.rate.deg.s <- motion.rot.rate.x.deg.s
     }
     
     if(mean(motion.accel.y.ms.2) < 0 & axis == 3) {
-      motion.accel.z.ms.2      <- -motion.accel.z.ms.2 
-      motion.accel.y.ms.2      <- -motion.accel.y.ms.2
-      motion.rot.rate.z.deg.s  <- -motion.rot.rate.z.deg.s
-      motion.rot.rate.y.deg.s  <- -motion.rot.rate.y.deg.s
+#       motion.accel.z.ms.2      <- -motion.accel.z.ms.2 
+#       motion.accel.y.ms.2      <- -motion.accel.y.ms.2
+#       motion.rot.rate.z.deg.s  <- -motion.rot.rate.z.deg.s
+#       motion.rot.rate.y.deg.s  <- -motion.rot.rate.y.deg.s
       
       print("Axis switched cyclic motion around z")
       
-      motion.rot.rate.deg.s <- motion.rot.rate.z.deg.s
+      # motion.rot.rate.deg.s <- motion.rot.rate.z.deg.s
     } 
     
     # Plot data
@@ -140,5 +145,6 @@ for (i in 1:nrow(fss.features)) {
     output.file.path <- paste(output.directory.path, body.position, "-motion-data-", measurement, ".csv", sep = "")
     write.csv(data.frame(t.ms, motion.accel.x.ms.2, motion.accel.y.ms.2, motion.accel.z.ms.2, motion.rot.rate.x.deg.s, motion.rot.rate.y.deg.s, motion.rot.rate.z.deg.s), output.file.path, row.names = FALSE)
     print(paste("Wrote:", output.file.path))
+    readline("Press return to continue") 
   }
 }
