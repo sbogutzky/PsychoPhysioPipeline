72e41a8ea1c5bb899c61a6a140b173514c25a9af
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-11-18 14:18:54 +0100

    Aufräumarbeiten

--- preprocessing/compute-motion-cardiolocomotor-phase-sychronization.R
+++ preprocessing/compute-motion-cardiolocomotor-phase-sychronization.R
@@ -4,15 +4,12 @@ rm(list = ls(all = T))
 # Load libraries
 require(flow)
 
-
 # Set root data directory path
-root.data.directory.path <- ""
-if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
-  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
-if(file.exists("/Volumes/flow/Documents/simon-bogutzky/data"))
-  root.data.directory.path <- "/Volumes/flow/Documents/simon-bogutzky/data/"
-if(file.exists("//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"))
-  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
+root.data.directory.path          <- ""
+if(file.exists("/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
+  root.data.directory.path        <- "/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
+if(file.exists("//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
+  root.data.directory.path        <- "//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
 
 # Set processed data directory path
 processed.data.directory.path <- paste(root.data.directory.path, "processed-data/", sep = "")
@@ -20,66 +17,78 @@ processed.data.directory.path <- paste(root.data.directory.path, "processed-data
 # Set features directory path
 features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
 
-# Load fss features
-fss.features <- read.csv(paste(features.directory.path, "fss-features.csv", sep = ""), stringsAsFactors = F)
+# Read activity directory
+activity.directory  <- readline("Type in activity directory and press return to continue (e. g. walking/) > ")
 
-# Set body position
-body.position       <- "leg"
+# Read user directory
+user.directory      <- readline("Type in user directory and press return to continue (e. g. doe-john/) > ")
+
+# Read in body position
+body.position       <- readline("Type in body position and press return to continue (e. g. leg) > ")
+
+# Set time range
+time.range  <- c(as.numeric(readline("Type in start for visualisation in seconds and press return to continue (e. g. 0) > ")), as.numeric(readline("Type in end for visualisation in seconds and press return to continue (e. g. 900) > ")))
+
+# Set time window
+t.w         <- as.numeric(readline("Type in time window in seconds and press return to continue (e. g. 30) > "))
+
+# Predicted relation
+m           <- as.numeric(readline("Type in predicted relation and press return to continue (e. g. 0.5) > "))
+
+# Load fss features
+fss.features        <- read.csv(paste(features.directory.path, activity.directory, user.directory, "fss-features.csv", sep = ""), stringsAsFactors = F)
 
 for (i in 1:nrow(fss.features)) {
-  
   properties      <- fss.features[i, c(6:12)]
-  activity        <- properties[, 1]
   activity.start  <- properties[, 2]
   activity.end    <- properties[, 3]
   measurement     <- properties[, 5]
-  last.name       <- properties[, 6]
-  first.name      <- properties[, 7]
   if(measurement == 1) {
     date.directory  <- paste(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d--%H-%M-%S"), "/", sep ="")
   }
   
-  # Read time data
-  motion.time.data.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, body.position, "-motion-time-data-", measurement, ".csv", sep="")
-  hrv.time.data.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, "hrv-time-data-", measurement, ".csv", sep="")
-  if(file.exists(motion.time.data.path) & file.exists(hrv.time.data.path)) {
+  # Set file paths
+  jerk.cost.data.file.path   <- paste(processed.data.directory.path, activity.directory, user.directory, date.directory, body.position, "-jerk-cost-data-", measurement, ".csv", sep="")
+  kubios.hrv.data.file.path  <- paste(processed.data.directory.path, activity.directory, user.directory, date.directory, "ecg-data-", measurement, "_hrv.txt", sep="")
+  
+  if(file.exists(jerk.cost.data.file.path) & file.exists(kubios.hrv.data.file.path)) {
     
-    # Load time data
-    motion.time.data <- read.csv(motion.time.data.path, skip = 2)
-    hrv.time.data <- read.csv(hrv.time.data.path, skip = 2)
+    # Load data
+    jerk.cost.data            <- read.csv(jerk.cost.data.file.path, skip = 2)
+    kubios.hrv.data.time.data <- read.csv(kubios.hrv.data.file.path, header = F, na.strings = "", fill = T, skip = 117, stringsAsFactors = F, col.names = c("", "t.s", "rr.interval.s", "", "", "", "", "", "", "", ""))[,2:3]
+    
+    # Data
+    t.l       <- jerk.cost.data$t.s
+    t.c       <- c(kubios.hrv.data.time.data$t.s[1] - kubios.hrv.data.time.data$rr.interval.s[1], kubios.hrv.data.time.data$t.s)
+    spm       <- 120 / jerk.cost.data$cycle.interval.s
+    bpm       <- 60 / kubios.hrv.data.time.data$rr.interval.s
     
     # Plot
-    par("mfcol" = c(4, 1), mar = c(2.5, 2.5, .5, 3.5) + 0.1, mgp = c(1.5, .5, 0), las = 1, cex.axis = 0.8, tck = .03, cex.lab = .8, xaxs = "i", yaxs = "i")
+    par(mfcol = c(4, 1), mar = c(3.5, 4, 2, 4) + 0.1, mgp = c(2.5, 1, 0))
     
     # BPM vs. SPM
-    t.l       <- motion.time.data$t.s
-    t.c       <- hrv.time.data$t.s
-    spm       <- 120 / motion.time.data$cycle.interval.s
-    bpm       <- 60 / hrv.time.data$rr.interval.s
-    x.lim     <- c(min(t.c), max(t.c))
-    plot(t.l, spm, xlab = "", ylab = "Cadence & HR", pch = 21, bg = "#3FADCB", cex = .5, xlim = x.lim, xaxt = "n")
-    points(t.c, bpm, pch = 22, bg = "#33D100", cex = .5)
-    abline(v = seq(x.lim[1], x.lim[2], 20), lty = "dashed", col = "lightgrey")
-    axis(1, at = seq(x.lim[1], x.lim[2], 20), labels = seq(x.lim[1], x.lim[2], 20), las = 1)
-    legend("bottomright", c("SPM", "BPM"), pch = c(21, 22), pt.bg = c("#3FADCB", "#33D100"), cex = .8, bg = "white")
+    plot(t.l, spm, xlab = "", ylab = "Cadence & HR", pch = 21, xlim = time.range, xaxt = "n")
+    points(t.c[-1], bpm, pch = 22)
+    abline(v = seq(time.range[1], time.range[2], t.w), lty = "dashed", col = "grey")
+    axis(1, at = seq(time.range[1], time.range[2], t.w), labels = seq(time.range[1], time.range[2], t.w), las = 1)
+    legend("topright", c("SPM", "BPM"), pch = c(21, 22), bg = "white")
     box()
+    
+    title(format(as.POSIXct(activity.start/1000, origin = "1970-01-01", tz = "CET"), "%Y-%m-%d %H:%M", tz = "CET"))
     
     # Stroboscopic Technique
     fi  <- CalculateInstantaneousPhases(t.c, t.l)
     t.c <- t.c[complete.cases(fi)]
     fi <- fi[complete.cases(fi)]
-    
-    m   <- .5
     psi <- fi %% (2 * pi * m)
     y.lim <- c(0, 1)
-    plot(t.c, psi / (2 * pi * m), xlab = "", ylab = expression(Psi[m](tk) / (2 * pi * m)), xaxt = "n",  yaxt = "n", xlim = x.lim, ylim = y.lim, pch = 21, bg = "#CB3FAD") # bg = rep(c("#3FADCB", "#CB3FAD"), length(psi)))
-    abline(v = seq(x.lim[1], x.lim[2], 20), lty = "dashed", col = "lightgrey")
-    axis(1, at = seq(x.lim[1], x.lim[2], 20), labels = seq(x.lim[1], x.lim[2], 20), las = 1)
+    plot(t.c, psi / (2 * pi * m), xlab = "", ylab = expression(Psi[m](tk) / (2 * pi * m)), xaxt = "n",  yaxt = "n", xlim = time.range, ylim = y.lim, pch = 21)
+    abline(v = seq(time.range[1], time.range[2], t.w), lty = "dashed", col = "grey")
+    axis(1, at = seq(time.range[1], time.range[2], t.w), labels = seq(time.range[1], time.range[2], t.w), las = 1)
     axis(2, at = seq(y.lim[1], y.lim[2], .2), labels = seq(y.lim[1], y.lim[2], .2))
     box()
     
     # Indexes
-    t.w <- 20
     t.s <- seq(min(t.c) + t.w/2, max(t.c) - t.w/2, 1)
     phase.coherence.indexes <- c()
     normalized.shannon.entropy.indexes <- c()
@@ -87,35 +96,36 @@ for (i in 1:nrow(fss.features)) {
       phase.coherence.indexes <- c(phase.coherence.indexes, CalculatePhaseCoherenceIndex(t.c, psi, t, t.w))
       normalized.shannon.entropy.indexes <- c(normalized.shannon.entropy.indexes, CalculateNormalizedShannonEntropyIndex(t.c, psi, t, t.w)) 
     }
-    plot(t.s, phase.coherence.indexes, type = "l",  xlab = "t[ s ]", ylab = "Indexes", xaxt = "n",  yaxt = "n", xlim = x.lim, ylim = y.lim, col = "#3FADCB")
+    plot(t.s, phase.coherence.indexes, type = "l",  xlab = "t[ s ]", ylab = "Indexes", xaxt = "n",  yaxt = "n", xlim = time.range, ylim = y.lim)
     lines(t.s, normalized.shannon.entropy.indexes, lty = 2)
-    abline(v = seq(x.lim[1], x.lim[2], 20), lty = "dashed", col = "lightgrey")
-    axis(1, at = seq(x.lim[1], x.lim[2], 20), labels = seq(x.lim[1], x.lim[2], 20), las = 1)
+    abline(v = seq(time.range[1], time.range[2], t.w), lty = "dashed", col = "grey")
+    axis(1, at = seq(time.range[1], time.range[2], t.w), labels = seq(time.range[1], time.range[2], t.w), las = 1)
     axis(2, at = seq(y.lim[1], y.lim[2], .2), labels = seq(y.lim[1], y.lim[2], .2))
-    legend("bottomright", c("Phase Coherence Index", "Shannon Entropy Index"), lty = c("solid", "dashed"), col = c("#3FADCB", "#000000"), cex = .8, bg = "white")
+    legend("topright", c("PCoI", "NSEI"), lty = c("solid", "dashed"),  bg = "white")
     box()
     
+    # Distribution
     hist(psi / (2 * pi * m), breaks = seq(0, 1, .05), col = "black", border = "white", main = "", xlab = expression(Psi[m] / (2 * pi * m)), ylab = "")
     
     # Create directory, if needed
-    output.directory.path <- paste(processed.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/", date.directory, sep="")
+    output.directory.path <- paste(processed.data.directory.path, activity.directory, user.directory, date.directory, sep="")
     if(!file.exists(output.directory.path)) {
       dir.create(output.directory.path, recursive = TRUE)
     }
     
     # Write csv file
-    output.file.path <- paste(output.directory.path, body.position, "-cps-indexes-", measurement, ".csv", sep = "")
+    output.file.path <- paste(output.directory.path, body.position, "-cls-indexes-", measurement, ".csv", sep = "")
     op <- options(digits.secs=3)
     con <- file(output.file.path, 'w') 
     writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d"), con = con)
     writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%H:%M:%OS"), con = con)
-    write.csv(data.frame(t.s, phase.coherence.index = phase.coherence.indexes, normalized.shannon.entropy.index = normalized.shannon.entropy.indexes), file = con, row.names = FALSE)
+    write.csv(data.frame(t.s, pcoi = phase.coherence.indexes, nsei = normalized.shannon.entropy.indexes), file = con, row.names = FALSE)
     close(con)
     options(op) #reset options
     print(paste("Wrote:", output.file.path))
     
     # Write csv file
-    output.file.path <- paste(output.directory.path,  body.position, "-cps-relative-phase-", measurement, ".csv", sep = "")
+    output.file.path <- paste(output.directory.path,  body.position, "-cls-relative-phase-", measurement, ".csv", sep = "")
     op <- options(digits.secs=3)
     con <- file(output.file.path, 'w') 
     writeLines(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y-%m-%d"), con = con)
@@ -128,4 +138,5 @@ for (i in 1:nrow(fss.features)) {
   } else {
     print("No data")
   }
+  readline("Press return to continue > ")
 }
