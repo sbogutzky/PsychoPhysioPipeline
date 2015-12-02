7ff03d2c518168e885739efd8e78c3b963dab5d7
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-12-02 13:24:11 +0100

    Skript verbessert

--- preprocessing/extract-fss-features.R
+++ preprocessing/extract-fss-features.R
@@ -6,12 +6,10 @@ library(flow)
 
 # Set root data directory path
 root.data.directory.path <- ""
-if(file.exists("C:/Users/Simon Bogutzky/Documents/flow/data"))
-  root.data.directory.path <- "C:/Users/Simon Bogutzky/Documents/flow/data/"
-if(file.exists("/Volumes/flow/Documents/simon-bogutzky/data"))
-  root.data.directory.path <- "/Volumes/flow/Documents/simon-bogutzky/data/"
-if(file.exists("//gangstore.ddns.net/flow/Documents/simon-bogutzky/data"))
-  root.data.directory.path <- "//gangstore.ddns.net/flow/Documents/simon-bogutzky/data/"
+if(file.exists("/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
+  root.data.directory.path        <- "/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
+if(file.exists("//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
+  root.data.directory.path        <- "//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
 
 # Set cleaned data directory path
 cleaned.data.directory.path <- paste(root.data.directory.path, "cleaned-data/", sep = "")
@@ -20,13 +18,16 @@ cleaned.data.directory.path <- paste(root.data.directory.path, "cleaned-data/", 
 features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
 
 # Set properties
-first.name        <- "Barbara"
-last.name         <- "Grueter"
-date.of.birth     <- "1950-04-15"
-activity          <- "Walking"
+first.name        <- "Patrick"
+last.name         <- "Buse"
+date.of.birth     <- "1984-05-05"
+activity          <- "Baseline"
+
+activity.directory <- paste(tolower(activity), "/",  sep = "")
+user.directory <- paste(tolower(last.name), "-", tolower(first.name), "/",  sep = "")
 
 # Load all file names
-input.data.directory.path <- paste(cleaned.data.directory.path, tolower(activity), "/", tolower(last.name), "-", tolower(first.name), "/",  sep = "")
+input.data.directory.path <- paste(cleaned.data.directory.path, activity.directory, user.directory, sep = "")
 fss.data.file.names <- list.files(path = input.data.directory.path, pattern = "fss-data.csv", recursive = T)
 
 # Create fss feature data frame
@@ -60,7 +61,7 @@ for (fss.data.file.name in fss.data.file.names) {
 }
 
 # Create output directory, if needed
-output.directory.path <- features.directory.path
+output.directory.path <- paste(features.directory.path, activity.directory, user.directory, sep = "")
 if(!file.exists(output.directory.path)) {
   dir.create(output.directory.path, recursive = TRUE)
 }
@@ -70,6 +71,8 @@ output.file.path <- paste(output.directory.path, "fss-features.csv", sep = "")
 if(file.exists(output.file.path)) {
   features <- read.csv(output.file.path, stringsAsFactors = FALSE)
   write.csv(unique(rbind(features, fss.features)), output.file.path, row.names = FALSE)
+  print(paste("Appended to", output.file.path))
 } else {
   write.csv(fss.features, output.file.path, row.names = FALSE)
+  print(paste("Worte to", output.file.path))
 }
