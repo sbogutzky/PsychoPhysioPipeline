fcf8b2f61e6426a571f2c3675e26ee4a0d12f3c8
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-12-03 21:05:50 +0100

    Entfernung von Duplikaten eingefügt

--- preprocessing/subset-ecg-data.R
+++ preprocessing/subset-ecg-data.R
@@ -10,6 +10,8 @@ if(file.exists("/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"
   root.data.directory.path        <- "/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
 if(file.exists("//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
   root.data.directory.path        <- "//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
+if(file.exists("C:/Users/Simon Bogutzky/Documents/Archiv/flow/data"))
+  root.data.directory.path        <- "C:/Users/Simon Bogutzky/Documents/Archiv/flow/data/"
 
 # Set cleaned data directory path
 cleaned.data.directory.path       <- paste(root.data.directory.path, "cleaned-data/", sep = "")
@@ -28,7 +30,6 @@ user.directory      <- readline("Type in user directory and press return to cont
 
 # Load fss features
 fss.features        <- read.csv(paste(features.directory.path, activity.directory, user.directory, "fss-features.csv", sep = ""), stringsAsFactors = F)
-
 
 for (i in 1:nrow(fss.features)) {
   
@@ -58,7 +59,7 @@ for (i in 1:nrow(fss.features)) {
       
       # Create output directory, if needed
       output.directory.path <- paste(preprocessed.data.directory.path, activity.directory, user.directory, date.directory, sep="")
-      if(!file.exists(output.directory.path)) {
+      if(!file.exists(substr(output.directory.path, 1, nchar(output.directory.path) - 1))) {
         dir.create(output.directory.path, recursive = TRUE)
       }
     } else {
@@ -70,35 +71,55 @@ for (i in 1:nrow(fss.features)) {
     
     # Subset ecg data
     ecg.data.subset   <- ecg.data[activity.start <= first.timestamp + ecg.data[,1] & first.timestamp + ecg.data[,1] < activity.end,]
-    time.difference   <- first.timestamp + ecg.data.subset[1,1] - activity.start
-    print(paste("Time difference (ms):", round(time.difference, 3)))
-    print(paste("Total time      (ms):", round(ecg.data.subset[nrow(ecg.data.subset),1] - ecg.data.subset[1,1] + time.difference, 3)))
-    
-    # Interpolate for Kubios HRV
     n.subset <- nrow(ecg.data.subset)
-    n.subset / ((ecg.data.subset[n.subset,1] - ecg.data.subset[1,1]) / 1000)
-    
-    # Determine fs
-    x   <- round(n.subset / ((ecg.data.subset[n.subset,1] - ecg.data.subset[1,1]) / 1000))
-    fs  <- 2^ceiling(log(x)/log(2))
-    print(paste("Sampling rate   (Hz):", fs))
-    
-    t.ms      <- seq(ecg.data.subset[1, 1], ecg.data.subset[n.subset, 1], by = 1000/fs)
-    lead.1.mv <- signal::interp1(ecg.data.subset[, 1], ecg.data.subset[,3] - ecg.data.subset[,2], t.ms, method = "spline")
-    lead.2.mv <- signal::interp1(ecg.data.subset[, 1], ecg.data.subset[,2], t.ms, method = "spline")
-    lead.3.mv <- signal::interp1(ecg.data.subset[, 1], ecg.data.subset[,3], t.ms, method = "spline")
-    t.ms      <- t.ms - t.ms[1] + time.difference
-    
-    # Plot data
-    par(mfcol=c(3, 1))
-    plot(t.ms[0:1024] / 1000, lead.1.mv[0:1024], type = "l", xlab = "t [s]", ylab = "Lead I [mV]")
-    title(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y/%m/%d %H:%M"))
-    plot(t.ms[0:1024] / 1000, lead.2.mv[0:1024], type = "l", xlab = "t [s]", ylab = "Lead II [mV]")
-    plot(t.ms[0:1024] / 1000, lead.3.mv[0:1024], type = "l", xlab = "t [s]", ylab = "Lead III [mV]")
-    
-    # Write csv file
-    output.file.path <- paste(output.directory.path, "ecg-data-", measurement, ".csv", sep = "")
-    write.csv(data.frame(t.ms, lead.1.mv, lead.2.mv, lead.3.mv), output.file.path, row.names = FALSE)
-    print(paste("Wrote:", output.file.path))
+    if(n.subset > 0) {
+      time.difference   <- first.timestamp + ecg.data.subset[1,1] - activity.start
+      print(paste("Time difference (ms):", round(time.difference, 3)))
+      print(paste("Total time      (ms):", round(ecg.data.subset[nrow(ecg.data.subset),1] - ecg.data.subset[1,1] + time.difference, 3)))
+      
+      output.file.path <- paste(output.directory.path, "ecg-data-", measurement, ".csv", sep = "")
+      
+      duplicates <- which(duplicated(ecg.data.subset))
+      if(length(duplicates) > 0) {
+        
+        t <- (ecg.data.subset$Timestamp - ecg.data.subset$Timestamp[1]) / 1000
+        plot(t, t, xlab = "t [s]", ylab = "t [s]")
+        points(t[duplicates], t[duplicates], col = "red")
+        
+        ecg.data.subset <- ecg.data.subset[-duplicates, ]
+        
+        print(paste(length(duplicates), "removed duplicates in ", output.file.path))
+        n.subset <- nrow(ecg.data.subset)
+        
+        #readline("Press return to continue > ")
+      }
+      
+      # Interpolate for Kubios HRV
+      n.subset / ((ecg.data.subset[n.subset,1] - ecg.data.subset[1,1]) / 1000)
+      
+      # Determine fs
+      x   <- round(n.subset / ((ecg.data.subset[n.subset,1] - ecg.data.subset[1,1]) / 1000))
+      fs  <- 2^ceiling(log(x)/log(2))
+      print(paste("Sampling rate   (Hz):", fs))
+      
+      t.ms      <- seq(ecg.data.subset[1, 1], ecg.data.subset[n.subset, 1], by = 1000/fs)
+      lead.1.mv <- signal::interp1(ecg.data.subset[, 1], ecg.data.subset[,3] - ecg.data.subset[,2], t.ms, method = "spline")
+      lead.2.mv <- signal::interp1(ecg.data.subset[, 1], ecg.data.subset[,2], t.ms, method = "spline")
+      lead.3.mv <- signal::interp1(ecg.data.subset[, 1], ecg.data.subset[,3], t.ms, method = "spline")
+      t.ms      <- t.ms - t.ms[1] + time.difference
+      
+      # Plot data
+      par(mfcol=c(3, 1))
+      plot(t.ms[0:1024] / 1000, lead.1.mv[0:1024], type = "l", xlab = "t [s]", ylab = "Lead I [mV]")
+      title(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y/%m/%d %H:%M"))
+      plot(t.ms[0:1024] / 1000, lead.2.mv[0:1024], type = "l", xlab = "t [s]", ylab = "Lead II [mV]")
+      plot(t.ms[0:1024] / 1000, lead.3.mv[0:1024], type = "l", xlab = "t [s]", ylab = "Lead III [mV]")
+      
+      # Write csv file
+      write.csv(data.frame(t.ms, lead.1.mv, lead.2.mv, lead.3.mv), output.file.path, row.names = F)
+      print(paste("Wrote:", output.file.path))
+    } else {
+      print(paste("No data in time range:", date.directory, measurement))
+    }
   }
 }
--- preprocessing/subset-motion-data.R
+++ preprocessing/subset-motion-data.R
@@ -10,6 +10,8 @@ if(file.exists("/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"
   root.data.directory.path        <- "/Volumes/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
 if(file.exists("//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen"))
   root.data.directory.path        <- "//gangstore.ddns.net/flow/Documents/archiv/daten/2015/flow-gehen-und-laufen/"
+if(file.exists("C:/Users/Simon Bogutzky/Documents/Archiv/flow/data"))
+  root.data.directory.path        <- "C:/Users/Simon Bogutzky/Documents/Archiv/flow/data/"
 
 # Set cleaned data directory path
 cleaned.data.directory.path       <- paste(root.data.directory.path, "cleaned-data/", sep = "")
@@ -80,7 +82,7 @@ for (i in 1:nrow(fss.features)) {
       
       # Create output directory, if needed
       output.directory.path <- paste(preprocessed.data.directory.path, activity.directory, user.directory, date.directory, sep="")
-      if(!file.exists(output.directory.path)) {
+      if(!file.exists(substr(output.directory.path, 1, nchar(output.directory.path) - 1))) {
         dir.create(output.directory.path, recursive = TRUE)
       }
     } else {
@@ -92,53 +94,73 @@ for (i in 1:nrow(fss.features)) {
     
     # Subset motion data
     motion.data.subset  <- motion.data[activity.start <= first.timestamp + motion.data[,1] & first.timestamp + motion.data[,1] < activity.end,]
-    time.difference     <- first.timestamp + motion.data.subset[1,1] - activity.start
-    print(paste("Time difference (ms):", round(time.difference, 3)))
-    print(paste("Total time      (ms):", round(motion.data.subset[nrow(motion.data.subset),1] - motion.data.subset[1,1] + time.difference, 3)))
-    
-    # Extract data
-    t.ms                <- motion.data.subset[,1] - motion.data.subset[1,1] + time.difference
-    acceleration.ms.2   <- t(matrix(c(motion.data.subset[, 2], motion.data.subset[, 3], motion.data.subset[, 4]), nrow(motion.data.subset), 3))
-    rotation.rate.deg.s <- t(matrix(c(motion.data.subset[, 5], motion.data.subset[, 6], motion.data.subset[, 7]), nrow(motion.data.subset), 3))
-    
-    # Find cyclic movement   
-    rotation.rate.x.spectrum <- TSA::periodogram(rotation.rate.deg.s[1, ], plot = F)
-    rotation.rate.y.spectrum <- TSA::periodogram(rotation.rate.deg.s[2, ], plot = F)
-    rotation.rate.z.spectrum <- TSA::periodogram(rotation.rate.deg.s[3, ], plot = F)
-    
-    axis <- which.max(c(max(rotation.rate.x.spectrum[[2]]), max(rotation.rate.y.spectrum[[2]]), max(rotation.rate.z.spectrum[[2]])))
-    
-    if(axis == 3) {
-      print("90 deg around y")
+    n.subset <- nrow(motion.data.subset)
+    if(n.subset > 0) {
+      time.difference     <- first.timestamp + motion.data.subset[1,1] - activity.start
+      print(paste("Time difference (ms):", round(time.difference, 3)))
+      print(paste("Total time      (ms):", round(motion.data.subset[nrow(motion.data.subset),1] - motion.data.subset[1,1] + time.difference, 3)))
       
-      # Rotation
-      rotation.matrix     <- matrix(c(cos(pi/2), 0, sin(pi/2), 0, 1, 0, -sin(pi/2), 0, cos(pi/2)), 3, 3, byrow = T)
-      acceleration.ms.2   <- rotation.matrix %*% acceleration.ms.2
-      rotation.rate.deg.s <- rotation.matrix %*% rotation.rate.deg.s
+      output.file.path <- paste(output.directory.path, body.position, "-motion-data-", measurement, ".csv", sep = "")
+      
+      duplicates <- which(duplicated(motion.data.subset))
+      if(length(duplicates) > 0) {
+        
+        t <- (motion.data.subsett$Timestamp - motion.data.subset$Timestamp[1]) / 1000
+        plot(t, t, xlab = "t [s]", ylab = "t [s]")
+        points(t[duplicates], t[duplicates], col = "red")
+        
+        motion.data.subset <- motion.data.subset[-duplicates, ]
+        
+        print(paste(length(duplicates), "removed duplicates in ", output.file.path))
+        
+        readline("Press return to continue > ")
+      }
+      
+      # Extract data
+      t.ms                <- motion.data.subset[,1] - motion.data.subset[1,1] + time.difference
+      acceleration.ms.2   <- t(matrix(c(motion.data.subset[, 2], motion.data.subset[, 3], motion.data.subset[, 4]), nrow(motion.data.subset), 3))
+      rotation.rate.deg.s <- t(matrix(c(motion.data.subset[, 5], motion.data.subset[, 6], motion.data.subset[, 7]), nrow(motion.data.subset), 3))
+      
+      # Find cyclic movement   
+      rotation.rate.x.spectrum <- TSA::periodogram(rotation.rate.deg.s[1, ], plot = F)
+      rotation.rate.y.spectrum <- TSA::periodogram(rotation.rate.deg.s[2, ], plot = F)
+      rotation.rate.z.spectrum <- TSA::periodogram(rotation.rate.deg.s[3, ], plot = F)
+      
+      axis <- which.max(c(max(rotation.rate.x.spectrum[[2]]), max(rotation.rate.y.spectrum[[2]]), max(rotation.rate.z.spectrum[[2]])))
+      
+      if(axis == 3) {
+        print("90 deg around y")
+        
+        # Rotation
+        rotation.matrix     <- matrix(c(cos(pi/2), 0, sin(pi/2), 0, 1, 0, -sin(pi/2), 0, cos(pi/2)), 3, 3, byrow = T)
+        acceleration.ms.2   <- rotation.matrix %*% acceleration.ms.2
+        rotation.rate.deg.s <- rotation.matrix %*% rotation.rate.deg.s
+      }
+      
+      if(mean(acceleration.ms.2[2, ]) < 0) {
+        print("180 deg around x")
+        
+        # Rotation
+        rotation.matrix     <- matrix(c(cos(pi), -sin(pi), 0, sin(pi), cos(pi), 0, 0, 0, 1), 3, 3, byrow = T)
+        acceleration.ms.2   <- rotation.matrix %*% acceleration.ms.2  
+        rotation.rate.deg.s <- rotation.matrix %*% rotation.rate.deg.s
+      } 
+      
+      # Plot data
+      #     plot(t.ms / 1000, acceleration.ms.2[1,], type = "l", xlab = "t [s]", ylab = "Acceleration X [ms^2]", xlim = c(140, 150))
+      #     plot(t.ms / 1000, acceleration.ms.2[2,], type = "l", xlab = "t [s]", ylab = "Acceleration Y [ms^2]", xlim = c(140, 150))
+      #     plot(t.ms / 1000, acceleration.ms.2[3,], type = "l", xlab = "t [s]", ylab = "Acceleration Z [ms^2]", xlim = c(140, 150))
+      plot(t.ms / 1000, rotation.rate.deg.s[1,], type = "l", xlab = "t [s]", ylab = "Rotation Rate X [deg/s]", xlim = c(140, 150))
+      #     plot(t.ms / 1000, rotation.rate.deg.s[2,], type = "l", xlab = "t [s]", ylab = "Rotation Rate Y [deg/s]", xlim = c(140, 150))
+      #     plot(t.ms / 1000, rotation.rate.deg.s[3,], type = "l", xlab = "t [s]", ylab = "Rotation Rate Z [deg/s]", xlim = c(140, 150))
+      title(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y/%m/%d %H:%M"))
+      
+      # Write csv file
+      write.csv(data.frame(t.ms, motion.accel.x.ms.2 = acceleration.ms.2[1, ], motion.accel.y.ms.2 = acceleration.ms.2[2, ], motion.accel.z.ms.2  = acceleration.ms.2[3, ], motion.rot.rate.x.deg.s = rotation.rate.deg.s[1, ], motion.rot.rate.y.deg.s  = rotation.rate.deg.s[2, ], motion.rot.rate.z.deg.s = rotation.rate.deg.s[3, ]), output.file.path, row.names = FALSE)
+      print(paste("Wrote:", output.file.path))
+    } else {
+      print(paste("No data in time range:", date.directory, measurement))
     }
-    
-    if(mean(acceleration.ms.2[2, ]) < 0) {
-      print("180 deg around x")
-      
-      # Rotation
-      rotation.matrix     <- matrix(c(cos(pi), -sin(pi), 0, sin(pi), cos(pi), 0, 0, 0, 1), 3, 3, byrow = T)
-      acceleration.ms.2   <- rotation.matrix %*% acceleration.ms.2  
-      rotation.rate.deg.s <- rotation.matrix %*% rotation.rate.deg.s
-    } 
-    
-    # Plot data
-#     plot(t.ms / 1000, acceleration.ms.2[1,], type = "l", xlab = "t [s]", ylab = "Acceleration X [ms^2]", xlim = c(140, 150))
-#     plot(t.ms / 1000, acceleration.ms.2[2,], type = "l", xlab = "t [s]", ylab = "Acceleration Y [ms^2]", xlim = c(140, 150))
-#     plot(t.ms / 1000, acceleration.ms.2[3,], type = "l", xlab = "t [s]", ylab = "Acceleration Z [ms^2]", xlim = c(140, 150))
-    plot(t.ms / 1000, rotation.rate.deg.s[1,], type = "l", xlab = "t [s]", ylab = "Rotation Rate X [deg/s]", xlim = c(140, 150))
-#     plot(t.ms / 1000, rotation.rate.deg.s[2,], type = "l", xlab = "t [s]", ylab = "Rotation Rate Y [deg/s]", xlim = c(140, 150))
-#     plot(t.ms / 1000, rotation.rate.deg.s[3,], type = "l", xlab = "t [s]", ylab = "Rotation Rate Z [deg/s]", xlim = c(140, 150))
-    title(strftime(as.POSIXct(activity.start / 1000, origin = "1970-01-01", tz="CET"), format="%Y/%m/%d %H:%M"))
-    
-    # Write csv file
-    output.file.path <- paste(output.directory.path, body.position, "-motion-data-", measurement, ".csv", sep = "")
-    write.csv(data.frame(t.ms, motion.accel.x.ms.2 = acceleration.ms.2[1, ], motion.accel.y.ms.2 = acceleration.ms.2[2, ], motion.accel.z.ms.2  = acceleration.ms.2[3, ], motion.rot.rate.x.deg.s = rotation.rate.deg.s[1, ], motion.rot.rate.y.deg.s  = rotation.rate.deg.s[2, ], motion.rot.rate.z.deg.s = rotation.rate.deg.s[3, ]), output.file.path, row.names = FALSE)
-    print(paste("Wrote:", output.file.path))
     readline("Press return to continue > ")
   }
 }
