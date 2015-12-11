676bb7a8bd7b11f29e2c820af86d4422fdbb818d
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-12-11 06:01:24 +0100

    Plot cycle means

--- preprocessing/detect-motion-cycles-compute-intervals-and-jerk-cost.R
+++ preprocessing/detect-motion-cycles-compute-intervals-and-jerk-cost.R
@@ -225,6 +225,29 @@ ComputeCycleJerkCosts <- function(A, mid.swing.indexes) {
   return(jerk.costs)
 }
 
+Mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+
+ComputeCycleMeanMatrix <- function(M, col, mid.swing.indexes) {
+  center <- Mode(diff(mid.swing.indexes))
+  values <- c()
+  j = 0
+  for(i in 1:(length(mid.swing.indexes) - 1)) {
+    m <- mid.swing.indexes[i]
+    n <- mid.swing.indexes[(i+1)]
+    values.temp <- M[, col][m:n]
+    if(center == length(values.temp)) {
+      values <- c(values, values.temp)
+      j = j + 1
+    }
+  }
+  M <- matrix(values, center, j)
+  
+  return(M)
+}
+
 saveData <- function(output.data, file.name, preprocessed.data.directory.path, activity.directory, user.directory, date.directory, activity.start) {
   
   # Create directory, if needed
@@ -327,16 +350,34 @@ for (i in 1:nrow(fss.features)) {
     A <- M[, 1:4]
     fn <- fs/2
     n <- 2
-    current.cycle <- A[, 1] > M[, 1][mid.swing.indexes[50]] & A[, 1] < M[, 1][mid.swing.indexes[51]]
     
+    mean.A.vertical <- ComputeCycleMeanMatrix(A, 3, mid.swing.indexes)
     par(mfcol = c(2, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
-    # plot(A[, 1][current.cycle] / 1000, A[, 2][current.cycle], type = "l", xlab = expression("Time ("~s~")"), ylab = expression("Acceleration ("~m/s^2~")"), ylim = c(-40, 30))
+    x.p <- 1:length(mean.A.vertical[, 1]) / length(mean.A.vertical[, 1]) * 100
+    plot(x.p,  mean.A.vertical[, 1], type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration vertical ("~m/s^2~")"))
+    for (i in 2:length(mean.A.vertical[1, ])) {
+      lines(x.p, mean.A.vertical[,i], type = "l")
+    }
+    mean.a.vertical <- rowMeans(mean.A.vertical)
+    plot(x.p, mean.a.vertical, type = "l",xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration vertical ("~m/s^2~")"))
+#     mean.a.vertical.shifted <- c(mean.a.vertical[(length(mean.A.vertical[, 1])/2 + 1):length(mean.A.vertical[, 1])], mean.a.vertical[1:(length(mean.A.vertical[, 1])/2)])
+#     plot(x.p, mean.a.vertical.shifted, xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration vertical ("~m/s^2~")"))
     
-    plot(A[, 1][current.cycle] / 1000, A[, 3][current.cycle], type = "l", xlab = expression("Time ("~s~")"), ylab = expression("Acceleration vertical ("~m/s^2~")"), ylim = c(-40, 30))
-    plot(A[, 1][current.cycle] / 1000, A[, 4][current.cycle], type = "l", xlab = expression("Time ("~s~")"), ylab = expression("Acceleration horizontal ("~m/s^2~")"), ylim = c(-40, 30))
+    mean.A.horizontal <- ComputeCycleMeanMatrix(A, 4, mid.swing.indexes)
+    par(mfcol = c(2, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
+    x.p <- 1:length(mean.A.horizontal[, 1]) / length(mean.A.horizontal[, 1]) * 100
+    plot(x.p,  mean.A.horizontal[, 1], type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration horizontal ("~m/s^2~")"))
+    for (i in 2:length(mean.A.horizontal[1, ])) {
+      lines(x.p, mean.A.horizontal[,i], type = "l")
+    }
+    mean.a.horizontal <- rowMeans(mean.A.horizontal)
+    plot(x.p, mean.a.horizontal, type = "l",xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration horizontal ("~m/s^2~")"))
+#     mean.a.horizontal.shifted <- c(mean.a.horizontal[(length(mean.A.horizontal[, 1])/2 + 1):length(mean.A.horizontal[, 1])], mean.a.horizontal[1:(length(mean.A.horizontal[, 1])/2)])
+#     plot(x.p, mean.a.horizontal.shifted, xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration horizontal ("~m/s^2~")"))
     
     # A[, 2] <- filtfilt(lowpass.filter, A[, 2])
     
+    par(mfcol = c(2, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
     fc <- 6.5
     W <- fc/fn
     lp.vertical <- butter(n, W)
@@ -351,11 +392,33 @@ for (i in 1:nrow(fss.features)) {
     plot(lp.horizontal.freg$f, abs(lp.horizontal.freg$h), type = "l", xlim = c(0, 30), xlab = "Frequency ( Hz )", ylab = "Magnitude Response")
     A[, 4] <- filtfilt(lp.horizontal, A[, 4])
     
-    par(mfcol = c(2, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
-    # plot(A[, 1][current.cycle] / 1000, A[, 2][current.cycle], type = "l", xlab = expression("Time ("~s~")"), ylab = expression("Acceleration ("~m/s^2~")"), ylim = c(-40, 30))
+    mean.A.vertical <- ComputeCycleMeanMatrix(A, 3, mid.swing.indexes)
+    par(mfcol = c(3, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
+    x.p <- 1:length(mean.A.vertical[, 1]) / length(mean.A.vertical[, 1]) * 100
+    plot(x.p,  mean.A.vertical[, 1], type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration vertical ("~m/s^2~")"))
+    for (i in 2:length(mean.A.vertical[1, ])) {
+      lines(x.p, mean.A.vertical[,i], type = "l")
+    }
+    mean.a.vertical <- rowMeans(mean.A.vertical)
+    plot(x.p, mean.a.vertical, type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration vertical ("~m/s^2~")"))
+#     mean.a.vertical.shifted <- c(mean.a.vertical[(length(mean.A.vertical[, 1])/2 + 1):length(mean.A.vertical[, 1])], mean.a.vertical[1:(length(mean.A.vertical[, 1])/2)])
+#     plot(x.p, mean.a.vertical.shifted, xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration vertical ("~m/s^2~")"))
+    mean.j.vertical <- c(NA, CalculateJerk(x.p, mean.a.vertical))
+    plot(x.p, mean.j.vertical, type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Jerk vertical ("~m/s^3~")"))
     
-    plot(A[, 1][current.cycle] / 1000, A[, 3][current.cycle], type = "l", xlab = expression("Time ("~s~")"), ylab = expression("Acceleration vertical ("~m/s^2~")"), ylim = c(-40, 30))
-    plot(A[, 1][current.cycle] / 1000, A[, 4][current.cycle], type = "l", xlab = expression("Time ("~s~")"), ylab = expression("Acceleration horizontal ("~m/s^2~")"), ylim = c(-40, 30))
+    mean.A.horizontal <- ComputeCycleMeanMatrix(A, 4, mid.swing.indexes)
+    par(mfcol = c(3, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
+    x.p <- 1:length(mean.A.horizontal[, 1]) / length(mean.A.horizontal[, 1]) * 100
+    plot(x.p,  mean.A.horizontal[, 1], type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration horizontal ("~m/s^2~")"))
+    for (i in 2:length(mean.A.horizontal[1, ])) {
+      lines(x.p, mean.A.horizontal[,i], type = "l")
+    }
+    mean.a.horizontal <- rowMeans(mean.A.horizontal)
+    plot(x.p, mean.a.horizontal, type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration horizontal ("~m/s^2~")"))
+#     mean.a.horizontal.shifted <- c(mean.a.horizontal[(length(mean.A.horizontal[, 1])/2 + 1):length(mean.A.horizontal[, 1])], mean.a.horizontal[1:(length(mean.A.horizontal[, 1])/2)])
+#     plot(x.p, mean.a.horizontal.shifted, xlab = expression("Time ( % Stride )"), ylab = expression("Acceleration horizontal ("~m/s^2~")"))
+    mean.j.horizontal <- c(NA, CalculateJerk(x.p, mean.a.horizontal))
+    plot(x.p, mean.j.horizontal, type = "l", xlab = expression("Time ( % Stride )"), ylab = expression("Jerk horizontal ("~m/s^3~")"))
     
     # Compute output data
     cycle.jerk.costs <- ComputeCycleJerkCosts(A, mid.swing.indexes)
