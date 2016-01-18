3809ae62ac309b753f8fb006090863fd109966ef
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-01-18 13:52:29 +0100

    Skript angepasst

--- preprocessing/extract-fss-features.R
+++ preprocessing/extract-fss-features.R
@@ -1,82 +1,72 @@
+# Version 2.0
+
 # Remove all variables
 rm(list = ls(all = T))  
 
 # Load libraries
 library(flow)
+library(multilevel)
 
-# Set root data directory path
-if(file.exists("/Volumes/OSX/flow/data"))
-  root.data.directory.path        <- "/Volumes/OSX/flow/data/"
+# Read root data directory
+root.directory <- readline("Type in root directory and press return to continue (with: /) > ")
 
-# Set cleaned data directory path
-cleaned.data.directory.path <- paste(root.data.directory.path, "cleaned-data/", sep = "")
+# Read properties
+first.name <- readline("Type in first name and press return to continue > ")
+last.name <- readline("Type in last name and press return to continue > ")
+date.of.birth <- readline("Type in date of birth and press return to continue (format: YYYY-MM-dd) > ")
+activity <- readline("Type in activity and press return to continue > ")
 
-# Set features directory path
-features.directory.path <- paste(root.data.directory.path, "features/", sep = "")
-
-# Set properties
-first.name        <- "Patrick"
-last.name         <- "Buse"
-date.of.birth     <- "1984-05-05"
-activity          <- "Running"
-
+# Set directories
+raw.data.directory <- paste(root.directory, "raw-data/", sep = "")
+feature.directory <- paste(root.directory, "features/", sep = "")
 activity.directory <- paste(tolower(activity), "/",  sep = "")
 user.directory <- paste(tolower(last.name), "-", tolower(first.name), "/",  sep = "")
+input.data.directory <- paste(raw.data.directory, activity.directory, user.directory, sep = "")
 
-# Load all file names
-input.data.directory.path <- paste(cleaned.data.directory.path, activity.directory, user.directory, sep = "")
-fss.data.file.names <- list.files(path = input.data.directory.path, pattern = "fss-data.csv", recursive = T)
+# Load all self report file names
+self.report.file.names <- list.files(path = input.data.directory, pattern = "self-report.csv", recursive = T)
 
-# Create fss feature data frame
-fss.features  <- data.frame()
-fss.measurements  <- data.frame()
+# Create fss feature and measurement data frame
+fss.features <- data.frame()
+fss.measurements <- data.frame()
 
-# Fill fss features data frame
-for (fss.data.file.name in fss.data.file.names) {
+for (self.report.file.name in self.report.file.names) {
   
-  # Load fss data
-  fss.data  <- read.csv(paste(input.data.directory.path, fss.data.file.name, sep = ""), header = T) 
+  # Load self report data
+  self.report.data <- read.csv(paste(input.data.directory, self.report.file.name, sep = ""), comment.char = "#") 
   
   # Loop measurements
-  for(i in 1:nrow(fss.data)) {
+  for(i in 1:nrow(self.report.data)) {
     
     # Extract times
     if(i == 1) {
-      activity.start  <- fss.data[i, 1] - 15 * 60000
+      activity.start.ms <- 0
     } else {
-      activity.start  <- inquiry.end
+      activity.start.ms <- self.report.stop.ms
     }
     
-    activity.end <- fss.data[i, 1]
-    inquiry.end  <- fss.data[i, 2]
+    activity.end.stop.ms <- self.report.data$timestamp.start.ms[i]
+    self.report.stop.ms <- self.report.data$timestamp.stop.ms[i]
     
     # Calculate fss factors
-    fss.factors     <- CalculateFlowShortScaleFactors(as.numeric(fss.data[i, 3:18]))
+    fss.measurement <- c(as.numeric(self.report.data[i, 7:15]), 0, 0, 0, as.numeric(self.report.data[i, 16:18]))
+    fss.factors <- CalculateFlowShortScaleFactors(fss.measurement)
     
-    # Add fss fss features
-    fss.features    <- rbind(fss.features, data.frame(round(fss.factors[c(1, 3, 5, 7, 9, 11)], 2), activity, activity.start, activity.end, inquiry.end, measurement = i, last.name, first.name, date.of.birth))
-    fss.measurements    <- rbind(fss.measurements, data.frame(fss.data[i, 3:18]))
+    # Add fss features
+    fss.features <- rbind(fss.features, data.frame(round(fss.factors[c(1, 3, 5, 7, 9, 11)], 2), activity, activity.start.ms, activity.end.stop.ms, self.report.stop.ms, measurement = i, last.name, first.name, date.of.birth))
+    fss.measurements <- rbind(fss.measurements, fss.measurement)
   }
 }
 
-print(paste("Cronbach's Alpha Flow", multilevel::cronbach(fss.measurements[, 1:10])$Alpha))
-print(paste("Cronbach's Alpha Fluency", multilevel::cronbach(fss.measurements[, c(8,7,9,4,5,2)])$Alpha))
-print(paste("Cronbach's Alpha Absorption", multilevel::cronbach(fss.measurements[, c(6,1,10,3)])$Alpha))
-print(paste("Cronbach's Alpha Anxienty", multilevel::cronbach(fss.measurements[, 11:13])$Alpha))
-
-# Create output directory, if needed
-output.directory.path <- paste(features.directory.path, activity.directory, user.directory, sep = "")
-if(!file.exists(substr(output.directory.path, 1, nchar(output.directory.path) - 1))) {
-  dir.create(output.directory.path, recursive = TRUE)
-}
+print(paste("Cronbach's Alpha Flow: ", multilevel::cronbach(fss.measurements[, 1:10])$Alpha))
+print(paste("Cronbach's Alpha Fluency: ", multilevel::cronbach(fss.measurements[, c(8,7,9,4,5,2)])$Alpha))
+print(paste("Cronbach's Alpha Absorption: ", multilevel::cronbach(fss.measurements[, c(6,1,10,3)])$Alpha))
 
 # Write to csv file
-output.file.path <- paste(output.directory.path, "fss-features.csv", sep = "")
-if(file.exists(output.file.path)) {
-  features <- read.csv(output.file.path, stringsAsFactors = FALSE)
-  write.csv(unique(rbind(features, fss.features)), output.file.path, row.names = FALSE)
-  print(paste("Appended to", output.file.path))
-} else {
-  write.csv(fss.features, output.file.path, row.names = FALSE)
-  print(paste("Worte to", output.file.path))
+output.directory <- paste(feature.directory, activity.directory, user.directory, sep = "")
+if(!file.exists(substr(output.directory, 1, nchar(output.directory) - 1))) {
+  dir.create(output.directory, recursive = T)
 }
+output.directory <- paste(output.directory, "fss-features.csv", sep = "")
+write.csv(fss.features, output.directory, row.names = F)
+print(paste("Worte to", output.directory))
