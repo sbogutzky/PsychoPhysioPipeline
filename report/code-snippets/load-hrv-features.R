1e96d77371e26086cad45ba9b375c5e271f6bd2e
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-02-16 16:46:43 +0100

    Daten Zusammenstellung für die Projektstudie

--- report/code-snippets/load-hrv-features.R
+++ report/code-snippets/load-hrv-features.R
@@ -1,18 +1,20 @@
 hrv.features  <- data.frame()
 for (i in 1:nrow(fss.features)) {
   source("code-snippets/set-additional-features.R")
-  hrv.feature.path <- paste(root.path, "features", "/", activity, "/", user, "/", "kubios-hrv-features", "/", strftime(date, format="%Y-%m-%d--%H-%M-%S"), "/", "ecg-data-", measurement, "_hrv.txt", sep = "")
+  hrv.feature.path <- paste(root.path, "features", "/", activity, "/", user, "/", "kubios-hrv-features", "/", strftime(date, format="%Y-%m-%d--%H-%M-%S"), "/", hrv.file.name.prefix, measurement, "_hrv.txt", sep = "")
   if(file.exists(hrv.feature.path)) {
     hrv.feature.rows <- read.csv(hrv.feature.path, skip = 0)
-    hrv.features <- rbind(hrv.features, hrv.feature.rows[, 1:46])
+    hrv.feature.rows <- hrv.feature.rows[, 1:46]
+    names(hrv.feature.rows) <- c("sample","meanrr","sdnn","meanhr","sdhr","rmssd","nn50","pnn50","sdann","sdnni","hrvtri","tinn","vlfpeakfft","lfpeakfft","hfpeakfft","vlfpowfft","lfpowfft","hfpowfft","vlfpowprfft","lfpowprfft","hfpowprfft","lfpownufft","hfpownufft","totpowfft","lfhffft","vlfpeakar","lfpeakar","hfpeakar","vlfpowar","lfpowar","hfpowar","vlfpowprar","lfpowprar","hfpowprar","lfpownuar","hfpownuar","totpowar","lfhfar","edr","sd1","sd2","apen","sampen","d2","dfa1","dfa2")
+    hrv.features <- rbind(hrv.features, hrv.feature.rows)
     rm(hrv.feature.rows)
   } else {
-    for (j in 1:3) {
-      hrv.feature.row <- rep(NA, 46)
+    for (j in 1:m) {
+      hrv.feature.row <- data.frame(t(rep(NA, 46)))
+      names(hrv.feature.row) <- c("sample","meanrr","sdnn","meanhr","sdhr","rmssd","nn50","pnn50","sdann","sdnni","hrvtri","tinn","vlfpeakfft","lfpeakfft","hfpeakfft","vlfpowfft","lfpowfft","hfpowfft","vlfpowprfft","lfpowprfft","hfpowprfft","lfpownufft","hfpownufft","totpowfft","lfhffft","vlfpeakar","lfpeakar","hfpeakar","vlfpowar","lfpowar","hfpowar","vlfpowprar","lfpowprar","hfpowprar","lfpownuar","hfpownuar","totpowar","lfhfar","edr","sd1","sd2","apen","sampen","d2","dfa1","dfa2")
       hrv.features <- rbind(hrv.features, hrv.feature.row)
     }
     rm(hrv.feature.row, j)
   }
 }
-names(hrv.features) <- c("sample","meanrr","sdnn","meanhr","sdhr","rmssd","nn50","pnn50","sdann","sdnni","hrvtri","tinn","vlfpeakfft","lfpeakfft","hfpeakfft","vlfpowfft","lfpowfft","hfpowfft","vlfpowprfft","lfpowprfft","hfpowprfft","lfpownufft","hfpownufft","totpowfft","lfhffft","vlfpeakar","lfpeakar","hfpeakar","vlfpowar","lfpowar","hfpowar","vlfpowprar","lfpowprar","hfpowprar","lfpownuar","hfpownuar","totpowar","lfhfar","edr","sd1","sd2","apen","sampen","d2","dfa1","dfa2")
-rm(hrv.feature.path, i, additional.features, activity.start, date, measurement)
+rm(hrv.feature.path, i, additional.features, activity.start, date, measurement, m, hrv.file.name.prefix, jc.file.name.prefix)
--- report/code-snippets/load-jc-features.R
+++ report/code-snippets/load-jc-features.R
@@ -1,7 +1,7 @@
 jc.features  <- data.frame()
 for (i in 1:nrow(fss.features)) {
   source("code-snippets/set-additional-features.R")
-  jc.data.path <- paste(root.path, "processed-data", "/", activity, "/", user, "/", strftime(date, format="%Y-%m-%d--%H-%M-%S"), "/", "leg-jerk-cost-data-", measurement, ".csv", sep = "")
+  jc.data.path <- paste(root.path, "processed-data", "/", activity, "/", user, "/", strftime(date, format="%Y-%m-%d--%H-%M-%S"), "/", jc.file.name.prefix, measurement, ".csv", sep = "")
   range.intervals <- seq(0, 900, 300)
   if(file.exists(jc.data.path)) {
     jc.data <- read.csv(jc.data.path, skip = 2)
@@ -10,16 +10,17 @@ for (i in 1:nrow(fss.features)) {
       jc.data.subset <- jc.data[jc.data[, 1] >= range.intervals[j] & jc.data[, 1] < range.intervals[j + 1], ]
       jc.data.subset <- jc.data.subset[jc.data.subset[, 2] < 1.25, ]
       jc.feature.row <- colMeans(jc.data.subset[, 2:3], na.rm = T)
+      names(jc.feature.row) <- c("mean.cycle.interval", "mean.jerk.cost")
       jc.features <- rbind(jc.features, jc.feature.row)
     }
     rm(jc.data, jc.data.subset, jc.feature.row, j)
   } else {
     for (j in 1:(length(range.intervals) - 1)) {
-      jc.feature.row <- rep(NA, 2)
+      jc.feature.row <- data.frame(t(rep(NA, 2)))
+      names(jc.feature.row) <- c("mean.cycle.interval", "mean.jerk.cost")
       jc.features <- rbind(jc.features, jc.feature.row)
     }
     rm(jc.feature.row, j)
   }
 }
-names(jc.features) <- c("mean.cycle.interval", "mean.jerk.cost")
-rm(jc.data.path, range.intervals, i, additional.features, activity.start, date, measurement)
+rm(jc.data.path, range.intervals, i, additional.features, activity.start, date, measurement, m, hrv.file.name.prefix, jc.file.name.prefix)
