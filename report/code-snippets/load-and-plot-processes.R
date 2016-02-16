d9f394aa4666e5919eb05e3fa0e73395fd046615
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-02-16 08:33:41 +0100

    Prozessdaten hinzugefügt / Unnötige Skripte gelöscht

--- report/code-snippets/load-and-plot-processes.R
+++ report/code-snippets/load-and-plot-processes.R
@@ -1,70 +1,79 @@
 # Set directory paths
-date.directory <- dir(path = paste(root.data.directory.path, "processed-data/", activity.directory, user.directory, sep = ""), pattern = paste(dates[i], "--[0-9]{2}-[0-9]{2}-[0-9]{2}", sep = ""))
-directory.path <- paste(root.data.directory.path, "processed-data/", activity.directory, user.directory, date.directory, "/", sep = "")
+directory.path <- dir(path = paste(root.path, "processed-data/", activities.1[i], "/", subjects[i], "/", sep = ""), pattern = paste(dates[i], "--[0-9]{2}-[0-9]{2}-[0-9]{2}", sep = ""), full.names = T)
 
 # Load heart data
-file.name <- paste("ecg-data-", m, "_hrv.txt", sep="")
-data.3 <- read.csv(paste(directory.path, file.name, sep = ""), header = F, na.strings = "", fill = T, skip = 117, stringsAsFactors = F, col.names = c("", "t.s", "rr.interval.s", "", "", "", "", "", "", "", ""))[,2:3]
+file.name <- paste("ecg-data-", as.numeric(measurements[i]), "_hrv.txt", sep="")
+process.data.1 <- read.csv(paste(directory.path, "/", file.name, sep = ""), header = F, na.strings = "", fill = T, skip = 117, stringsAsFactors = F, col.names = c("", "t.s", "rr.interval.s", "", "", "", "", "", "", "", ""))[,2:3]
 
 # Create hrv data
 require(RHRV)
-data.4 <- CreateHRVData()
-data.4 <- SetVerbose(data.4, F)
-data.4$Beat <- data.frame("Time" = data.3$t.s)
+process.data.2 <- CreateHRVData()
+process.data.2 <- SetVerbose(process.data.2, F)
+process.data.2$Beat <- data.frame("Time" = process.data.1$t.s)
 
 # Build not interpolated heart rates
-data.4 <- BuildNIHR(data.4)
+process.data.2 <- BuildNIHR(process.data.2)
 
 # Linear Interpolate the data by 4 Hz (default)
-data.4 <- InterpolateNIHR(data.4)
+process.data.2 <- InterpolateNIHR(process.data.2)
 
 # Create Frequency analysis (CWT) with least asymmetric Daubechies of width 8 for ULF, VLF, LF and HF 
-data.4 <- CreateFreqAnalysis(data.4)
-data.4 <- CalculatePowerBand(data.4, indexFreqAnalysis=1, type="wavelet", wavelet="la8", bandtolerance=0.005, ULFmin=0, ULFmax=0.0033, VLFmin=0.0033, VLFmax=0.04, LFmin=0.04, LFmax=0.15, HFmin=0.15, HFmax=0.4)
+process.data.2 <- CreateFreqAnalysis(process.data.2)
+process.data.2 <- CalculatePowerBand(process.data.2, indexFreqAnalysis=1, type="wavelet", wavelet="la8", bandtolerance=0.005, ULFmin=0, ULFmax=0.0033, VLFmin=0.0033, VLFmax=0.04, LFmin=0.04, LFmax=0.15, HFmin=0.15, HFmax=0.4)
 
 # Load CLS data
-file.name <- paste("leg-cls-relative-phase-", m, ".csv", sep = "")
-data.5 <- read.csv(paste(directory.path, file.name, sep = ""), skip = 2)
+file.name <- paste("leg-cls-phases-", as.numeric(measurements[i]), ".csv", sep = "")
+process.data.3 <- read.csv(paste(directory.path, "/", file.name, sep = ""), skip = 2)
+file.name <- paste("leg-cls-indexes-", as.numeric(measurements[i]), ".csv", sep = "")
+process.data.4 <- read.csv(paste(directory.path, "/", file.name, sep = ""), skip = 2)
 
 # Load JC data
-file.name <- paste("leg-jerk-cost-data-", m, ".csv", sep = "")
-data.6 <- read.csv(paste(directory.path, file.name, sep = ""), skip = 2)
+file.name <- paste("leg-jerk-cost-data-", as.numeric(measurements[i]), ".csv", sep = "")
+process.data.5 <- read.csv(paste(directory.path, "/", file.name, sep = ""), skip = 2)
 
-par(mfcol = c(5, 1), mar = c(3.5, 4, 2, 4) + 0.1, mgp = c(2.5, 1, 0))
+# Compute spm and bpm
+spm       <- 60 / (process.data.5$cycle.interval.s / 2)
+bpm       <- 60 / process.data.1$rr.interval.s
 
-# Plot HR
-t.s <- seq(min(data.3$t.s),max(data.3$t.s), length.out = length(data.4$HR))
-plot(t.s, data.4$HR, xlab = "", ylab = "HR (BPM)", xlim = time.range, ylim = c(100, 120), pch = 21, xaxt = "n")
+par("mfcol" = c(4, 1), mar = c(2.5, 2.5, .5, 3.5) + 0.1, mgp = c(1.5, .5, 0), las = 1, cex.axis = 0.8, tck = .03, cex.lab = .8, xaxs = "i", yaxs = "i")
+
+# Plot cadance and hr
+plot(process.data.5$t.s, spm, xlab = "", ylab = "Cadence & HR", xaxt = "n", xlim = time.range, ylim = c(90, 130), pch = 21, bg = "#3FADCB")
+points(process.data.1$t.s, bpm, pch = 22, bg = "#33D100")
 axis(1, at = ticks, labels = F)
 abline(v = ticks, lty = "dashed", col = "darkgrey")
-box()
-
-# Set title
-title(paste(dates[i], "30'"))
-
-# Plot Cylce Intervals
-plot(data.6$t.s, data.6$cycle.interval.s, xlab = "", ylab = "Cycle Interval (s)", xlim = time.range, ylim = c(1, 1.15), pch = 21, xaxt = "n")
-axis(1, at = ticks, labels = F)
-abline(v = ticks, lty = "dashed", col = "darkgrey")
+legend("bottomright", c("SPM", "BPM"), pch = c(21, 22), pt.bg = c("#3FADCB", "#33D100"), cex = .8, bg = "white")
 box()
 
 # Plot CLS
-plot(data.5[, 1], data.5[, 2], xlab = "", ylab = expression(Psi[m](tk) / (2 * pi * m)), xlim = time.range, ylim = c(0, 1), pch = 21, xaxt = "n")
+plot(process.data.3$t.s, process.data.3$psi, xlab = "", ylab = expression(Psi(t)), xaxt = "n", xlim = time.range, ylim = c(0, 1), pch = 21, bg = "#3FADCB")
 axis(1, at = ticks, labels = F)
 abline(v = ticks, lty = "dashed", col = "darkgrey")
 box()
 
+frame()
+
+plot(process.data.4$t.s, process.data.4$pcoi, type = "l", xlab = "Time (s)", ylab = "Indexes", xlim = time.range, ylim = c(0, 1), lty = 2)
+lines(process.data.4$t.s, process.data.4$nsei, col = "#3FADCB")
+axis(1, at = ticks, labels = F)
+abline(v = ticks, lty = "dashed", col = "darkgrey")
+legend("topleft", c("Coherence Index", "Shannon Entropy Index"), lty = c("solid", "dashed"), col = c("#3FADCB", "#000000"), cex = .8, bg = "white")
+box()
+
+par("mfcol" = c(2, 1), mar = c(2.5, 2.5, .5, 3.5) + 0.1, mgp = c(1.5, .5, 0), las = 1, cex.axis = 0.8, tck = .03, cex.lab = .8, xaxs = "i", yaxs = "i")
+
 # Plot HF
-plot(t.s, data.4$FreqAnalysis[[1]]$HF, xlab = "", ylab = expression("HF ("~ms^2~")"), xlim = time.range, ylim = c(0, 50), pch = 21, xaxt = "n")
+t.s <- seq(min(process.data.1$t.s), max(process.data.1$t.s), length.out = length(process.data.2$HR))
+plot(t.s, process.data.2$FreqAnalysis[[1]]$HF, xlab = "", ylab = expression("HF ("~ms^2~")"), type = "l", xlim = time.range, ylim = c(0, 100), pch = 21, xaxt = "n", col = "#3FADCB")
 axis(1, at = ticks, labels = F)
 abline(v = ticks, lty = "dashed", col = "darkgrey")
 box()
 
 # Plot JC
 require(caTools)
-plot(data.6$t.s, runmean(data.6$jerk.cost.m2s5/10^4, 10, endrule = "NA"), xlab = "", ylab = expression("JC (x"~10^4~m^2*s^{-5}~")"), xlim = time.range, ylim = c(1, 1.75), pch = 21, xaxt = "n")
+plot(process.data.5$t.s, runmean(process.data.5$jerk.cost.m2s5/10^4, 10, endrule = "NA"), type = "l", xlab = "", ylab = expression("JC (x"~10^4~m^2*s^{-5}~")"), xlim = time.range, ylim = c(1, 2.5), pch = 21, col = "#3FADCB")
 axis(1, at = ticks)
 abline(v = ticks, lty = "dashed", col = "darkgrey")
 box()
 
-rm(data.3, data.4, data.5, data.6, file.name, t.s)
+rm(process.data.1, process.data.2, process.data.3, process.data.4, process.data.5, directory.path, file.name, bpm, spm, t.s)
