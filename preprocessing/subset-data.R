d97b855027d7c8c9052d5078ba704a02904a9632
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-03-02 12:22:32 +0100

    Erster Selbstreport wird ignoriert

--- preprocessing/subset-data.R
+++ preprocessing/subset-data.R
@@ -17,6 +17,7 @@ for (self.report.file.name in self.report.file.names) {
   
   # Load self report data
   self.report.data <- read.csv(paste(input.data.directory, self.report.file.name, sep = ""), comment.char = "#")
+  self.report.data <- self.report.data[-1,]
   
   data.path <- paste(input.data.directory, date.directory, data.file.name, ".csv", sep = "")
   
