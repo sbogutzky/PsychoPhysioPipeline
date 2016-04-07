7440c171e16fcbc235d0f8f8611e2f28c42852d5
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-04-07 16:57:08 +0200

    Kleine Korrektur

--- preprocessing/detect-mid-swings.R
+++ preprocessing/detect-mid-swings.R
@@ -67,8 +67,8 @@ for (self.report.file.name in self.report.file.names) {
         output.data <- data.frame(timestamp.ms = data$timestamp.ms[mid.swing.indexes], row = mid.swing.indexes)
         write.csv(output.data, output.directory, row.names = F)
         print(paste("Wrote:", output.directory))
-        print(paste("First step: ", round(min(output.data$timestamp.ms / 1000) / 60), "m ",  round(min(output.data$timestamp.ms / 1000) %% 60), "s", sep = ""))
-        print(paste("Last step: ", round(max(output.data$timestamp.ms / 1000) / 60), "m ",  round(max(output.data$timestamp.ms / 1000) %% 60), "s", sep = ""))
+        print(paste("First step: ", round(min(output.data$timestamp.ms / 1000)), "s", sep = ""))
+        print(paste("Last step: ", round(max(output.data$timestamp.ms / 1000)), "s", sep = ""))
       } else {
         print("No mid swings detected")
       } 
