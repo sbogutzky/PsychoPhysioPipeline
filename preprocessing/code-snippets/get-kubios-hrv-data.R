46bd8e8e33a02a6d0f848b14102c420800c45234
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-06-11 00:38:21 +0200

    Skript überarbeitet

---
+++ preprocessing/code-snippets/get-kubios-hrv-data.R
@@ -0,0 +1,35 @@
+kubios.hrv.data <- read.csv(kubios.hrv.data.file.path, header = F, na.strings = "", fill = T, skip = 117, stringsAsFactors = F)
+
+kubios.hrv.data <- kubios.hrv.data[, 3:ncol(kubios.hrv.data)-1]
+
+samples <- ncol(kubios.hrv.data) / 9
+
+data <- kubios.hrv.data[, 1:2]
+if(samples > 1) {
+  for (k in 2:samples) {
+    m <- ((k-1) * 9) + 1
+    n <- m + 1
+    
+    data.1 <- kubios.hrv.data[, m:n]
+    names(data.1) <- names(data) 
+    data <- rbind(data, data.1)
+    
+    m <- ((k-1) * 2) + 1
+    n <- m + 1
+    
+    o <- which(kubios.hrv.data[, 3] == 2) + 1
+    p <- nrow(kubios.hrv.data)
+    
+    data.1 <- kubios.hrv.data[o:p, m:n]
+    names(data.1) <- names(data) 
+    data <- rbind(data, data.1)
+    
+  }
+  rm(samples, k, m, n, o, p, data.1)
+  data <- data[complete.cases(data), ]
+}
+
+kubios.hrv.data <- data
+rm(data)
+names(kubios.hrv.data) <- c("t.s", "rr.interval.s")
+kubios.hrv.data <- kubios.hrv.data[order(kubios.hrv.data$t.s),]
--- preprocessing/compute-motion-cardiolocomotor-phase-sychronization.R
+++ preprocessing/compute-motion-cardiolocomotor-phase-sychronization.R
@@ -4,143 +4,140 @@
 rm(list = ls(all = T))  
 
 # Load libraries
-require(flow)
+library(flow)
+library(zoom)
 
 # Set working directory
 setwd("~/psychophysiopipeline/preprocessing")
 
-# Use code snippets
-source("./code-snippets/read-set-load.R")
-
-# Set input file names
-data.file.name.1 <- readline("Type in data file name of the ecg file and press return to continue > ")
-data.file.name.2 <- readline("Type in data file name of the first motion file and press return to continue > ")
-data.file.name.3 <- readline("Type in data file name of the second motion file and press return to continue (optional) > ")
-
-# Set time window
-time.window.s <- as.numeric(readline("Type in time window in seconds and press return to continue (e. g. 30) > "))
+# User input
+root.directory.path <- readline("Quellverzeichnis > ")  
+first.name <- readline("Vorname der Untersuchungsperson > ")
+last.name <- readline("Nachname der Untersuchungsperson > ")
+activity <-  readline("Aktivität der Untersuchung > ")
+ecg.data.file.name <- readline("Dateiname der Datei mit EKG-Daten (ohne .csv) > ")
+kinematic.data.file.name.1 <- readline("Dateiname der ersten Datei mit kinematischen Daten (ohne .csv) > ")
+kinematic.data.file.name.2 <- readline("Dateiname der zweiten Datei mit kinematischen Daten (ohne .csv / optimal) > ")
+time.window.s <- as.numeric(readline("Zeitfenster für die Index-Berechnung  (s) > "))
 if(is.na(time.window.s)) time.window.s = 30
+
+# Set directory paths
+source("./code-snippets/set-directory-paths.R")
+
+# List self report names
+self.report.file.names <- list.files(path = raw.data.directory.path, pattern = "self-report.csv", recursive = TRUE)
 
 for (self.report.file.name in self.report.file.names) {
   
-  source("./code-snippets/extract-session-start.R")
+  source("./code-snippets/get-session-start.R")
   
   # Load self report data
-  self.report.data <- read.csv(paste(input.data.directory, self.report.file.name, sep = ""), comment.char = "#")
+  self.report.data <- read.csv(paste(raw.data.directory.path, self.report.file.name, sep = ""), comment.char = "#")
   
   # Loop measurements
   for(i in 1:nrow(self.report.data)) {
     
-    source("./code-snippets/extract-self-report-times.R")
+    source("./code-snippets/get-self-report-times.R")
     
-    mid.swing.data.file.path.1 <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, data.file.name.2, "-mid-swing-indexes-", i, ".csv", sep="")
-    if(data.file.name.3 != "") mid.swing.data.file.path.2 <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, data.file.name.3, "-mid-swing-indexes-", i, ".csv", sep="")
-    kubios.hrv.data.file.path <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, data.file.name.1, "-", i, "_hrv.txt", sep="")
+    stride.data.file.path.1 <- paste(processed.data.directory.path, date.directory, kinematic.data.file.name.1, "-stride-data-", i,  ".csv", sep = "")
+    if(kinematic.data.file.name.2 != "") stride.data.file.path.2 <- paste(processed.data.directory.path, date.directory, kinematic.data.file.name.2, "-stride-data-", i,  ".csv", sep = "")
+    kubios.hrv.data.file.path <- paste(processed.data.directory.path, date.directory, ecg.data.file.name, "-", i, "_hrv.txt", sep="")
   
-    if(file.exists(mid.swing.data.file.path.1) & file.exists(kubios.hrv.data.file.path)) {
+    if(file.exists(stride.data.file.path.1) & file.exists(kubios.hrv.data.file.path)) {
       
-      # Load step times
-      mid.swing.data <- read.csv(mid.swing.data.file.path.1, skip = 0)
+      # Set stride times
+      stride.data.1 <- read.csv(stride.data.file.path.1, skip = 0)
       m <- 2
-      if(data.file.name.3 != "") {
-        mid.swing.data.2 <- read.csv(mid.swing.data.file.path.2, skip = 0)
-        mid.swing.data <- rbind(mid.swing.data, mid.swing.data.2)
+      if(kinematic.data.file.name.2 != "" & file.exists(stride.data.file.path.2)) {
+        stride.data.2 <- read.csv(stride.data.file.path.2, skip = 0)
+        stride.data.1 <- rbind(stride.data.1, stride.data.2)
         m <- 1
       }
-      mid.swing.data <- mid.swing.data[order(mid.swing.data$timestamp.ms),]
-      step.times <- mid.swing.data$timestamp.ms / 1000
+      stride.data.1 <- stride.data.1[order(stride.data.1[, 1]),]
+      stride.times <- stride.data.1[, 1] / 1000
       
       # Load heart beat times
-      source("./code-snippets/read-kubios-hrv-data.R")
-      heart.beat.times <- c(kubios.hrv.data.time.data$t.s[1] - kubios.hrv.data.time.data$rr.interval.s[1], kubios.hrv.data.time.data$t.s)
+      source("./code-snippets/get-kubios-hrv-data.R")
+      heart.beat.times <- c(kubios.hrv.data[1, 1] - kubios.hrv.data[1, 2], kubios.hrv.data[, 1])
       
-      time.range.s <- c(round(heart.beat.times[1]) - 10, round(heart.beat.times[length(heart.beat.times)]) + 10)
+      spm <- c(NA, 60 * m / diff(stride.times)) 
+      bpm <- c(NA, 60 / diff(heart.beat.times))
       
-      spm <- 60 * m / diff(step.times) 
-      bpm <- 60 / diff(heart.beat.times) 
-      y.lim.1 <- c(min(mean(spm) - sd(spm) * 4, mean(bpm) - sd(bpm) * 4), max(mean(spm) + sd(spm) * 4, mean(bpm) + sd(bpm) * 4))
+      time.range.s <- c(round(min(heart.beat.times)) - 10, round(max(heart.beat.times)) + 10)
+      y.lim <- c(min(mean(spm, na.rm = TRUE) - sd(spm, na.rm = TRUE) * 2, mean(bpm, na.rm = TRUE) - sd(bpm, na.rm = TRUE) * 2), max(mean(spm, na.rm = TRUE) + sd(spm, na.rm = TRUE) * 2, mean(bpm, na.rm = TRUE) + sd(bpm, na.rm = TRUE) * 2))
       
       # Plot
       par(mfcol = c(3, 1), mar = c(3.5, 4, 2, 4) + 0.1, mgp = c(2.5, 1, 0))
-      y.lim <- c(0, 1)
       
       # SPM vs. BPM 
-      plot(step.times[-1], spm, xlab = "", ylab = "Cadence & HR", xaxt = "n", xlim = time.range.s, ylim = y.lim.1, xaxs = "i", pch = 23, bg = "deepskyblue")
-      points(heart.beat.times[-1], bpm, pch = 21, bg = "tomato")
-      abline(v = seq(time.range.s[1], time.range.s[2], time.window.s), lty = "dashed", col = "grey")
+      plot(stride.times, spm, xlab = "", ylab = "Mittlerer Schritt & Mittlerer HR", xaxt = "n", xlim = time.range.s, ylim = y.lim, xaxs = "i", pch = 23, bg = rgb(0/255, 152/255, 199/255))
+      points(heart.beat.times, bpm, pch = 21, bg = rgb(229/255, 66/255, 66/255))
+      abline(v = seq(time.range.s[1], time.range.s[2], time.window.s), lty = "dashed", col = rgb(186/255, 187/255, 194/255))
       axis(1, at = seq(time.range.s[1], time.range.s[2], time.window.s), labels = seq(time.range.s[1], time.range.s[2], time.window.s), las = 1)
-      legend("topright", c("SPM", "BPM"), pch = c(23, 21), pt.bg = c("deepskyblue", "tomato"), bg = "white")
+      legend("topright", c("SPM", "BPM"), pch = c(23, 21), pt.bg = c(rgb(0/255, 152/255, 199/255), rgb(229/255, 66/255, 66/255)), bg = "white")
       box()
       
-      title(paste(format(session.start, "%Y/%m/%d %H:%M", tz = "CET"), " #", i, sep = ""))
+      title(paste(format(session.start + activity.start.ms / 1000, "%d.%m.%Y %H:%M", tz = "CET"), " #", i, sep = ""))
       
       # Stroboscopic Technique
-      instantaneous.phase.data <- ComputeInstantaneousPhases(heart.beat.times, step.times)
-      t <- instantaneous.phase.data$ts
-      fi <- instantaneous.phase.data$fi # instantaneous phases
+      instantaneous.phase.data <- ComputeInstantaneousPhases(heart.beat.times, stride.times)
+      fi <- instantaneous.phase.data[, 2] # instantaneous phases
       psi <- (fi %% (2 * pi)) / (2 * pi) # relative phases
+      cls.phase.data <- data.frame(timestamp.ms = round(instantaneous.phase.data[, 1] * 1000, 3), round(fi, 3), round(psi, 3))
+      rm(instantaneous.phase.data, fi, psi)
       
-      plot(t, psi, xlab = "", ylab = expression(Psi(t)), xaxt = "n",  yaxt = "n", xlim = time.range.s, xaxs = "i", yaxs = "i", ylim = y.lim, pch = 21, bg = "darkolivegreen1")
-      abline(v = seq(time.range.s[1], time.range.s[2], time.window.s), lty = "dashed", col = "grey")
+      y.lim <- c(0, 1)
+      plot(cls.phase.data[, 1] / 1000, cls.phase.data[, 3], xlab = "", ylab = expression("Rel. Phase " ~ Psi(t)), xaxt = "n",  yaxt = "n", xlim = time.range.s, xaxs = "i", yaxs = "i", ylim = y.lim, pch = 24, bg = rgb(96/255, 65/255, 79/255))
+      abline(v = seq(time.range.s[1], time.range.s[2], time.window.s), lty = "dashed", col = rgb(186/255, 187/255, 194/255))
       axis(1, at = seq(time.range.s[1], time.range.s[2], time.window.s), labels = seq(time.range.s[1], time.range.s[2], time.window.s), las = 1)
       axis(2, at = seq(y.lim[1], y.lim[2], .2), labels = seq(y.lim[1], y.lim[2], .2))
       box()
       
-      # Indexes
-      time.points <- seq(min(t) + time.window.s/2, max(t) - time.window.s/2, 1)
+      # Compute Indexes
+      timestamps <- seq(min(cls.phase.data[, 1] / 1000) + time.window.s/2, max(cls.phase.data[, 1] / 1000) - time.window.s/2, 1)
       phase.coherence.indexes <- c()
       normalized.shannon.entropy.indexes <- c()
-      for (time.point in time.points) {
-        phase.coherence.indexes <- c(phase.coherence.indexes, ComputePhaseCoherenceIndex(t, psi, time.point, time.window.s))
-        normalized.shannon.entropy.indexes <- c(normalized.shannon.entropy.indexes, ComputeNormalizedShannonEntropyIndex(t, psi, time.point, time.window.s)) 
+      for (timestamp in timestamps) {
+        phase.coherence.indexes <- c(phase.coherence.indexes, ComputePhaseCoherenceIndex(cls.phase.data[, 1]/ 1000, cls.phase.data[, 3], timestamp, time.window.s))
+        normalized.shannon.entropy.indexes <- c(normalized.shannon.entropy.indexes, ComputeNormalizedShannonEntropyIndex(cls.phase.data[, 1]/ 1000, cls.phase.data[, 3], timestamp, time.window.s)) 
       }
-      rm(time.point)
+      rm(timestamp)
       
-      indexes <- data.frame(t.s = time.points, pcoi = phase.coherence.indexes, nsei = normalized.shannon.entropy.indexes)
-      indexes <- indexes[indexes$t.s > heart.beat.times[1] & indexes$t.s < heart.beat.times[length(heart.beat.times)], ]
+      cls.index.data <- data.frame(timestamp.ms = timestamps * 1000, pcoi = round(phase.coherence.indexes, 3), nsei = round(normalized.shannon.entropy.indexes, 3))
+      cls.index.data <- cls.index.data[cls.index.data[, 1] / 1000 > min(heart.beat.times) & cls.index.data[, 1] / 1000 < max(heart.beat.times), ]
+      rm(timestamps, phase.coherence.indexes, normalized.shannon.entropy.indexes)
       
-      plot(indexes$t.s, indexes$nsei, type = "l", xlab = "t (s)", ylab = "Indexes", xaxt = "n",  yaxt = "n", xlim = time.range.s, xaxs = "i", yaxs = "i", ylim = y.lim, col = "deepskyblue")
-      lines(indexes$t.s, indexes$pcoi, lty = 2)
-      abline(v = seq(time.range.s[1], time.range.s[2], time.window.s), lty = "dashed", col = "grey")
+      plot(cls.index.data[, 1] / 1000, cls.index.data[, 3], type = "l", xlab = "Zeit (s)", ylab = "Indexes", xaxt = "n",  yaxt = "n", xlim = time.range.s, xaxs = "i", yaxs = "i", ylim = y.lim, col = rgb(0/255, 152/255, 199/255))
+      lines(cls.index.data[, 1] / 1000, cls.index.data[, 2], lty = 2)
+      abline(v = seq(time.range.s[1], time.range.s[2], time.window.s), lty = "dashed", col = rgb(186/255, 187/255, 194/255))
       axis(1, at = seq(time.range.s[1], time.range.s[2], time.window.s), labels = seq(time.range.s[1], time.range.s[2], time.window.s), las = 1)
       axis(2, at = seq(y.lim[1], y.lim[2], .2), labels = seq(y.lim[1], y.lim[2], .2))
-      legend("topright", c("PCoI", "NSEI"), lty = c("dashed", "solid"),  col = c("black", "deepskyblue"), bg = "white")
+      legend("topright", c("PCoI", "NSEI"), lty = c("dashed", "solid"),  col = c("black", rgb(0/255, 152/255, 199/255)), bg = "white")
       box()
+      zm()
       
-      # Distribution
-      # hist(psi, breaks = seq(0, 1, .05), col = "black", border = "white", main = "", xlab = expression(Psi),  xaxs = "i", yaxs = "i", ylab = "")
+      # Print synchronisation features
+      print("---")
+      print(paste("Mittlerer normalisierter Shannon Entropie Index:", round(mean(cls.index.data[, 2], na.rm = TRUE), 2)))
+      print(paste("Mittlerer Phasenkohärenz:", round(mean(cls.index.data[, 3], na.rm = TRUE), 2)))
       
-      # Create directory, if needed
-      output.directory.path <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, sep="")
-      if(!file.exists(substr(output.directory.path, 1, nchar(output.directory.path) - 1))) {
-        dir.create(output.directory.path, recursive = TRUE)
+      # Write to csv file
+      if(!dir.exists(paste(processed.data.directory.path, date.directory, sep =""))) {
+        dir.create(paste(processed.data.directory.path, date.directory, sep =""), recursive = T)
       }
-
-      # Write csv file
-      output.file.path <- paste(output.directory.path, "cls-indexes-", i, ".csv", sep = "")
-      op <- options(digits.secs=3)
-      con <- file(output.file.path, 'w')
-      writeLines(strftime(session.start, format="%Y-%m-%d"), con = con)
-      writeLines(strftime(session.start, format="%H:%M:%OS"), con = con)
-      write.csv(indexes, file = con, row.names = FALSE)
-      close(con)
-      options(op) #reset options
-      print(paste("Wrote:", output.file.path))
-
-      # Write csv file
-      output.file.path <- paste(output.directory.path, "cls-phases-", i, ".csv", sep = "")
-      op <- options(digits.secs=3)
-      con <- file(output.file.path, 'w')
-      writeLines(strftime(session.start, format="%Y-%m-%d"), con = con)
-      writeLines(strftime(session.start, format="%H:%M:%OS"), con = con)
-      write.csv(data.frame(t.s = t, fi, psi), file = con, row.names = FALSE)
-      close(con)
-      options(op) #reset options
-      print(paste("Wrote:", output.file.path))
+      write.csv(cls.index.data, paste(processed.data.directory.path, date.directory, "cls-index-data-", i, ".csv", sep = ""), row.names = F)
+      write.csv(cls.phase.data, paste(processed.data.directory.path, date.directory, "cls-phase-data-", i, ".csv", sep = ""), row.names = F)
+      
+      print("---")
+      print(paste(paste("cls-index-data-", i, ".csv", sep = ""), "in", paste(processed.data.directory.path, date.directory, sep =""), "geschrieben."))
+      print(paste(paste("cls-phase-data-", i, ".csv", sep = ""), "in", paste(processed.data.directory.path, date.directory, sep =""), "geschrieben."))
       
     } else {
-      print("No data")
+      print("---")
+      print(paste("Datei nicht gefunden:", ecg.data.file.name))
+      print("oder")
+      print(paste("Datei nicht gefunden:", kinematic.data.1.file.path))
     }
-    readline("Press return to continue > ")
+    readline("Weiter > ")
   }
 }
