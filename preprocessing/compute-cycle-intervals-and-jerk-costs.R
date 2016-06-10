ea3ea2d1f7f706ea0a7d8dbc8ee2c61ba09ecf29
Simon Bogutzky <simon.bogutzky@t-online.de>
2016-06-10 21:29:32 +0200

    Skript überarbeitet

--- preprocessing/compute-cycle-intervals-and-jerk-costs.R
+++ preprocessing/compute-cycle-intervals-and-jerk-costs.R
@@ -4,157 +4,109 @@
 rm(list = ls(all = T))  
 
 # Load libraries
-require(flow)
-require(signal)
+library(flow)
+library(zoom)
+library(signal)
 
 # Set working directory
 setwd("~/psychophysiopipeline/preprocessing")
 
-source("./code-snippets/read-set-load.R")
+# User input
+root.directory.path <- readline("Quellverzeichnis > ") 
+first.name <- readline("Vorname der Untersuchungsperson > ")
+last.name <- readline("Nachname der Untersuchungsperson > ")
+activity <- readline("Aktivität der Untersuchung > ")
+kinematic.data.file.name <- readline("Dateiname der Datei mit kinematischen Daten (ohne .csv) > ")
+cut.off.frequency.x <- as.numeric(readline("Grenzfrequenz für die Beschleunigung entlang der X-Achse (Hz) > "))
+cut.off.frequency.y <- as.numeric(readline("Grenzfrequenz für die Beschleunigung entlang der Y-Achse (Hz) > "))
+cut.off.frequency.z <- as.numeric(readline("Grenzfrequenz für die Beschleunigung entlang der Z-Achse (Hz) > "))
 
-data.file.name <- readline("Type in data file name and press return to continue > ")
-sampling.rate <- as.numeric(readline("Type in sampling rate and press return to continue > "))
+# Set directory paths
+source("./code-snippets/set-directory-paths.R")
 
-optimal.cutoff.frequencies <- c()
-count <- 0
+# List self report names
+self.report.file.names <- list.files(path = raw.data.directory.path, pattern = "self-report.csv", recursive = TRUE)
+
 for (self.report.file.name in self.report.file.names) {
   
-  source("./code-snippets/extract-session-start.R")
+  source("./code-snippets/get-session-start.R")
   
   # Load self report data
-  self.report.data <- read.csv(paste(input.data.directory, self.report.file.name, sep = ""), comment.char = "#")
+  self.report.data <- read.csv(paste(raw.data.directory.path, self.report.file.name, sep = ""), comment.char = "#")
   
   # Loop measurements
   for(i in 1:nrow(self.report.data)) {
     
-    source("./code-snippets/extract-self-report-times.R")
-    data.path <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, data.file.name, "-", i,  ".csv", sep = "")
+    source("./code-snippets/get-self-report-times.R")
     
-    if(file.exists(data.path)) {
+    kinematic.data.file.path <- paste(processed.data.directory.path, date.directory, kinematic.data.file.name, "-", i,  ".csv", sep = "")
     
-      # Compute optimal cutoff frequencies
-      data <- read.csv(data.path)
+    stride.data.file.path <- paste(processed.data.directory.path, date.directory, kinematic.data.file.name, "-stride-data-", i,  ".csv", sep = "")
     
-      A <- data[, 1:4]
-      n <- 4
-      count <- count + 1
-      optimal.cutoff.frequencies <- c(optimal.cutoff.frequencies, ComputeOptimalCutoffFrequency(A[, 2], sampling.rate, n), ComputeOptimalCutoffFrequency(A[, 3], sampling.rate, n), ComputeOptimalCutoffFrequency(A[, 4], sampling.rate, n))
-    }
-  }
-}
-
-optimal.cutoff.frequencies <- rowMeans(matrix(optimal.cutoff.frequencies, 3, count), na.rm = T)
-
-for (self.report.file.name in self.report.file.names) {
-  
-  source("./code-snippets/extract-session-start.R")
-  
-  # Load self report data
-  self.report.data <- read.csv(paste(input.data.directory, self.report.file.name, sep = ""), comment.char = "#")
-  
-  #Loop measurements
-  for(i in 1:nrow(self.report.data)) {
-    
-    source("./code-snippets/extract-self-report-times.R")
-    
-    data.path <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, data.file.name, "-", i,  ".csv", sep = "")
-    mid.swing.path <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, data.file.name,"-mid-swing-indexes-", i,  ".csv", sep = "")
-    
-    if(file.exists(data.path) & file.exists(mid.swing.path)) {
+    if(file.exists(kinematic.data.file.path) & file.exists(stride.data.file.path)) {
       
-      # Load motion data and midswing indexes
-      data <- read.csv(data.path)
-      mid.swing.indexes <- read.csv(mid.swing.path)[, 2]
-      A <- data[, 1:4]
+      # Load kinematic data and stride data
+      kinematic.data <- read.csv(kinematic.data.file.path)
+      stride.data <- read.csv(stride.data.file.path)
       
-      # Plot raw acceleration
-      PlotMostCommonCycleAcceleration(A[, 2], mid.swing.indexes, main = "Coronary")
-      PlotMostCommonCycleAcceleration(A[, 3], mid.swing.indexes, main = "Sagittal")
-      PlotMostCommonCycleAcceleration(A[, 4], mid.swing.indexes, main = "Axial")
+      # Compute sampling rate
+      fs <- ComputeSamplingRate(kinematic.data[, 1])
       
       # Smooth acceleration
-      fn <- sampling.rate/2
+      fn <- fs/2
       n <- 4
       
-      par(mfcol = c(3, 1), mar = c(3.5, 4, 3.5, 4) + 0.1, mgp = c(2.5, 1, 0))
-      
-      fc <- optimal.cutoff.frequencies[1]
+      fc <- cut.off.frequency.x
       W <- fc/fn
-      lp.coronary <- butter(n, W)
-      lp.coronary.freg <- freqz(lp.coronary, Fs = sampling.rate)
-      plot(lp.coronary.freg$f, abs(lp.coronary.freg$h), type = "l", xlim = c(0, 30), xlab = "Frequency (Hz)", ylab = "Magnitude Response", main = "Coronary acceleration filter")
-      A[, 2] <- filtfilt(lp.coronary, A[, 2])
+      lp <- butter(n, W)
+      acceleration.x <- filtfilt(lp, kinematic.data[, 2])
       
-      fc <- optimal.cutoff.frequencies[2]
+      fc <- cut.off.frequency.y
       W <- fc/fn
-      lp.sagittal <- butter(n, W)
-      lp.sagittal.freg <- freqz(lp.sagittal, Fs = sampling.rate)
-      plot(lp.sagittal.freg$f, abs(lp.sagittal.freg$h), type = "l", xlim = c(0, 30), xlab = "Frequency (Hz)", ylab = "Magnitude Response", main = "Sagittal acceleration filter")
-      A[, 3] <- filtfilt(lp.sagittal, A[, 3])
+      lp <- butter(n, W)
+      acceleration.y <- filtfilt(lp, kinematic.data[, 3])
       
-      fc <- optimal.cutoff.frequencies[3]
+      fc <- cut.off.frequency.z
       W <- fc/fn
-      lp.axial <- butter(n, W)
-      lp.axial.freg <- freqz(lp.axial, Fs = sampling.rate)
-      plot(lp.axial.freg$f, abs(lp.axial.freg$h), type = "l", xlim = c(0, 30), xlab = "Frequency (Hz)", ylab = "Magnitude Response", main = "Axial acceleration filter")
-      A[, 4] <- filtfilt(lp.axial, A[, 4])
+      lp <- butter(n, W)
+      acceleration.z <- filtfilt(lp, kinematic.data[, 4])
+      rm(fs, fn, n, fc, W, lp)
       
-      # Plot filtered acceleration
-      PlotMostCommonCycleAcceleration(A[, 2], mid.swing.indexes, main = "Coronary")
-      PlotMostCommonCycleAcceleration(A[, 3], mid.swing.indexes, main = "Sagittal")
-      PlotMostCommonCycleAcceleration(A[, 4], mid.swing.indexes, main = "Axial")
-      
-      # Compute output data
-      t.s <- A[, 1][mid.swing.indexes] / 1000
-      cycle.interval.s <- diff(t.s)
-      t.s <- t.s[-1]
-      
-      jerk.cost.m2s5 <- c()
-      for(j in 1:(length(mid.swing.indexes) - 1)) {
-        
-        m <- mid.swing.indexes[j]
-        n <- mid.swing.indexes[(j+1)]
-        
-        # Compute jerk cost of each cycle
-        t.ms                <- A[, 1][m:n]
-        acceleration.x.ms.2 <- A[, 2][m:n]
-        acceleration.y.ms.2 <- A[, 3][m:n]
-        acceleration.z.ms.2 <- A[, 4][m:n]
-        
-        jerk.cost   <- ComputeJerkCost(t.ms / 1000, data.frame(acceleration.x.ms.2, acceleration.y.ms.2, acceleration.z.ms.2), normalized = T)
-        jerk.cost.m2s5  <- c(jerk.cost.m2s5, jerk.cost)
+      jerk.cost <- c(NA)
+      for(j in 1:(length(stride.data[, 1]) - 1)) {
+        stride.indexes <- which(stride.data[j, 1] <= kinematic.data[, 1] & kinematic.data[, 1] < stride.data[j + 1, 1])
+        if(stride.data[j + 1, 2] < 13000) {
+          jerk.cost <- c(jerk.cost, ComputeJerkCost(kinematic.data[, 1][stride.indexes] / 1000, data.frame(acceleration.x[stride.indexes], acceleration.y[stride.indexes], acceleration.z[stride.indexes]), normalized = T))
+        } else {
+          jerk.cost <- c(jerk.cost, NA)
+        }
       }
+      jerk.cost.data <- data.frame(timestamp.ms = stride.data[, 1], jerk.cost.m2s5 = round(jerk.cost, 3))
+      rm(j, acceleration.x, acceleration.y, acceleration.z)
       
-      output.data <- data.frame(t.s, cycle.interval.s, jerk.cost.m2s5)
-      # output.data <- output.data[cycle.interval.s < 1.25,]
+      # Compute stride features
+      mean.stride <- mean(60 / (stride.data[, 2] / 1000), na.rm = TRUE)
+      mean.jerk.cost <- mean(jerk.cost.data[, 2], na.rm = TRUE)
       
-      # Detect outliers
-      # anomaly <- DetectAnomaly(cycle.interval.s, jerk.cost.m2s5 / 10^5, "Cycle Interval (s)", expression("JC (x"~10^5~m^2*s^{-5}~")"), c(min(cycle.interval.s), max(cycle.interval.s)), c(min(jerk.cost.m2s5 / 10^5), max(jerk.cost.m2s5 / 10^5)))
-      # if(length(anomaly$outliers) > 0) {
-      #   output.data <- output.data[-anomaly$outliers, ]
-      # }
+      # Print stride features
+      print("---")
+      print(paste("Mittlerer Doppelschritt:", round(mean.stride, 2), "1/min"))
+      print(paste("Mittlerer Bewegungsaufwand:", round(mean.jerk.cost, 2), "m^2/s^-5"))
       
-      # Compute mean to compare
-      jerk.cost.by.all.accelerations <- ComputeJerkCost(A[, 1] / 1000, A[, 2:4], normalized = T) 
-      jerk.cost.by.cycle.mean <- mean(output.data[, 3], na.rm = T)
-      print(paste("Jerk Cost by Cycle:            (x10^3 m2s5)", jerk.cost.by.cycle.mean / 10^3))
-      print(paste("Jerk Cost by all Accelerations (x10^3 m2s5):", jerk.cost.by.all.accelerations / 10^3))
-      
-      # Create directory, if needed
-      output.directory <- paste(preprocessed.data.directory, activity.directory, user.directory, date.directory, sep="")
-      if(!file.exists(substr(output.directory, 1, nchar(output.directory) - 1))) {
-        dir.create(output.directory, recursive = TRUE)
+      # Write to csv file
+      if(!dir.exists(paste(processed.data.directory.path, date.directory, sep =""))) {
+        dir.create(paste(processed.data.directory.path, date.directory, sep =""), recursive = T)
       }
+      write.csv(jerk.cost.data, paste(processed.data.directory.path, date.directory, kinematic.data.file.name, "-jerk-cost-data-", i, ".csv", sep = ""), row.names = F)
       
-      # Write csv file
-      output.file.name <- paste(data.file.name, "-cycle-intervals-jerk-costs-", i, ".csv", sep = "")
-      output.directory <- paste(output.directory, output.file.name, sep = "")
-      write.csv(output.data, output.directory, row.names = F)
-      print(paste("Wrote:", output.directory))
+      print("---")
+      print(paste(paste(kinematic.data.file.name, "-jerk-cost-data-", i, ".csv", sep = ""), "in", paste(processed.data.directory.path, date.directory, sep =""), "geschrieben."))
   
     } else {
-      print(paste("No files found:", data.path))
+      print("---")
+      print(paste("Datei nicht gefunden:", kinematic.data.file.path))
+      print("oder")
+      print(paste("Datei nicht gefunden:", stride.data.file.path))
     }
-    readline("Press return to continue > ")
   }
 }
